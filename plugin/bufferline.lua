c775939eaa28dc5036ce55b9848c12edde345da8
ayamir <lgt986452565@gmail.com>
2021-06-23 15:59:40 +0800

    [plugin] disable bufferline in dashboard

--- plugin/bufferline.lua
+++ plugin/bufferline.lua
@@ -13,12 +13,16 @@ require "bufferline".setup {
 		tab_size = 20,
 		show_tab_indicators = true,
 		diagnostics = "nvim_lsp",
-	enforce_regular_tabs = false,
-	view = "multiwindow",
-	show_buffer_close_icons = true,
-	always_show_bufferline = true,
-	separator_style = "thin",
-	mappings = "true"
+		enforce_regular_tabs = false,
+		view = "multiwindow",
+		show_buffer_close_icons = true,
+		always_show_bufferline = false,
+		separator_style = "thin",
+		mappings = "true",
+		custom_filter = function(buf_number)
+			if vim.fn.bufname(buf_number) ~= "dashboard" then
+				return true
+			end
+		end,
 	}
-	}
-
+}
