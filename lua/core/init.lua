dff1c3c7c98a15e9035c738d9f8d920b9efe4da5
ayamir <lgt986452565@gmail.com>
2021-07-20 21:10:20 +0800

    [init] disable correct.

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -21,24 +21,24 @@ local createdir = function()
 end
 
 local disable_distribution_plugins = function()
-    vim.g.loaded_gzip = 1
-    vim.g.loaded_tar = 1
-    vim.g.loaded_tarPlugin = 1
-    vim.g.loaded_zip = 1
-    vim.g.loaded_zipPlugin = 1
-    vim.g.loaded_getscript = 1
-    vim.g.loaded_getscriptPlugin = 1
-    vim.g.loaded_vimball = 1
-    vim.g.loaded_vimballPlugin = 1
-    vim.g.loaded_matchit = 1
-    vim.g.loaded_matchparen = 1
-    vim.g.loaded_2html_plugin = 1
-    vim.g.loaded_logiPat = 1
-    vim.g.loaded_rrhelper = 1
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-    vim.g.loaded_netrwSettings = 1
-    vim.g.loaded_netrwFileHandlers = 1
+    vim.g.loaded_gzip = 0
+    vim.g.loaded_tar = 0
+    vim.g.loaded_tarPlugin = 0
+    vim.g.loaded_zip = 0
+    vim.g.loaded_zipPlugin = 0
+    vim.g.loaded_getscript = 0
+    vim.g.loaded_getscriptPlugin = 0
+    vim.g.loaded_vimball = 0
+    vim.g.loaded_vimballPlugin = 0
+    vim.g.loaded_matchit = 0
+    vim.g.loaded_matchparen = 0
+    vim.g.loaded_2html_plugin = 0
+    vim.g.loaded_logiPat = 0
+    vim.g.loaded_rrhelper = 0
+    vim.g.loaded_netrw = 0
+    vim.g.loaded_netrwPlugin = 0
+    vim.g.loaded_netrwSettings = 0
+    vim.g.loaded_netrwFileHandlers = 0
 end
 
 local leader_map = function()
