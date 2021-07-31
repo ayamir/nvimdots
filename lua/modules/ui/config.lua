b8b61b0070864ad432bc700a037ffaa7cad4a7bb
ayamir <lgt986452565@gmail.com>
2021-07-31 22:19:50 +0800

    [lualine] update config.

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -11,12 +11,34 @@ end
 function config.lualine()
     require('lualine').setup {
         options = {
+            icons_enabled = true,
             theme = 'github',
-            -- For round icons (require Nerd-Font)
-            section_separators = {"", ""}
-            -- component_separators = {"", ""},
-            -- ... your lualine config
-        }
+            disabled_filetypes = {}
+        },
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch'},
+            lualine_c = {
+                {'filename'}, {
+                    'diagnostics',
+                    sources = {'nvim_lsp'},
+                    symbols = {error = ' ', warn = ' ', info = ' '}
+                }
+            },
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+        },
+        tabline = {},
+        extensions = {}
     }
 end
 
