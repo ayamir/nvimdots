f612e51eefbf190401945470f998c7b00df76e56
ayamir <lgt986452565@gmail.com>
2021-08-04 13:00:09 +0800

    [plugin] fix indent-blankline.

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -236,31 +236,33 @@ function config.gitsigns()
 end
 
 function config.indent_blankline()
-    -- vim.cmd [[highdark IndentOne guifg=#BF616A guibg=NONE gui=nocombine]]
-    -- vim.cmd [[highdark IndentTwo guifg=#D08770 guibg=NONE gui=nocombine]]
-    -- vim.cmd [[highdark IndentThree guifg=#EBCB8B guibg=NONE gui=nocombine]]
-    -- vim.cmd [[highdark IndentFour guifg=#A3BE8C guibg=NONE gui=nocombine]]
-    -- vim.cmd [[highdark IndentFive guifg=#5E81AC guibg=NONE gui=nocombine]]
-    -- vim.cmd [[highdark IndentSix guifg=#88C0D0 guibg=NONE gui=nocombine]]
-    -- vim.cmd [[highdark IndentSeven guifg=#B48EAD guibg=NONE gui=nocombine]]
-    -- vim.g.indent_blankline_char_highdark_list = {
-    --     "IndentOne", "IndentTwo", "IndentThree", "IndentFour", "IndentFive",
-    --     "IndentSix", "IndentSeven"
-    -- }
-    vim.g.indent_blankline_char = "│"
-    vim.g.indent_blankline_show_first_indent_level = true
-    vim.g.indent_blankline_filetype_exclude = {
-        "startify", "dashboard", "dotooagenda", "log", "fugitive", "gitcommit",
-        "packer", "vimwiki", "markdown", "json", "txt", "vista", "help",
-        "todoist", "NvimTree", "peekaboo", "git", "TelescopePrompt", "undotree",
-        "flutterToolsOutline", "" -- for all buffers without a file type
+    vim.cmd [[highlight IndentTwo guifg=#D08770 guibg=NONE gui=nocombine]]
+    vim.cmd [[highlight IndentThree guifg=#EBCB8B guibg=NONE gui=nocombine]]
+    vim.cmd [[highlight IndentFour guifg=#A3BE8C guibg=NONE gui=nocombine]]
+    vim.cmd [[highlight IndentFive guifg=#5E81AC guibg=NONE gui=nocombine]]
+    vim.cmd [[highlight IndentSix guifg=#88C0D0 guibg=NONE gui=nocombine]]
+    vim.cmd [[highlight IndentSeven guifg=#B48EAD guibg=NONE gui=nocombine]]
+    vim.g.indent_blankline_char_highlight_list = {
+        "IndentTwo", "IndentThree", "IndentFour", "IndentFive", "IndentSix",
+        "IndentSeven"
     }
-    vim.g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
-    vim.g.indent_blankline_show_trailing_blankline_indent = false
-    vim.g.indent_blankline_show_current_context = true
-    vim.g.indent_blankline_context_patterns = {
-        "class", "function", "method", "block", "list_literal", "selector",
-        "^if", "^table", "if_statement", "while", "for"
+    require("indent_blankline").setup {
+        char = "│",
+        show_first_indent_level = true,
+        filetype_exclude = {
+            "startify", "dashboard", "dotooagenda", "log", "fugitive",
+            "gitcommit", "packer", "vimwiki", "markdown", "json", "txt",
+            "vista", "help", "todoist", "NvimTree", "peekaboo", "git",
+            "TelescopePrompt", "undotree", "flutterToolsOutline", "" -- for all buffers without a file type
+        },
+        buftype_exclude = {"terminal", "nofile"},
+        show_trailing_blankline_indent = false,
+        show_current_context = true,
+        context_patterns = {
+            "class", "function", "method", "block", "list_literal", "selector",
+            "^if", "^table", "if_statement", "while", "for", "type", "var",
+            "import"
+        }
     }
     -- because lazy load indent-blankline so need readd this autocmd
     vim.cmd('autocmd CursorMoved * IndentBlanklineRefresh')
