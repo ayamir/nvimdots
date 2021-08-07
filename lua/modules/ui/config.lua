9e07328116d8f868bb9911603ce65102985f9648
ayamir <lgt986452565@gmail.com>
2021-08-07 21:32:58 +0800

    [gitsign] add config.

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -216,15 +216,27 @@ function config.gitsigns()
             },
 
             ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+            ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
             ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
             ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+            ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+            ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
             ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-            ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+            ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
 
             -- Text objects
             ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
             ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
-        }
+        },
+        watch_index = {interval = 1000, follow_files = true},
+        current_line_blame = true,
+        current_line_blame_delay = 1000,
+        current_line_blame_position = 'eol',
+        sign_priority = 6,
+        update_debounce = 100,
+        status_formatter = nil, -- Use default
+        word_diff = false,
+        use_internal_diff = true -- If luajit is present
     }
 end
 
