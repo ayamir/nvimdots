72104f7d3feda3b69f35dcaed2d04f70bf6ed1bb
ayamir <lgt986452565@gmail.com>
2021-08-10 12:13:25 +0800

    [editor] add config for treesitter-textobject.

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -94,6 +94,26 @@ function config.nvim_treesitter()
                     ["ac"] = "@class.outer",
                     ["ic"] = "@class.inner"
                 }
+            },
+            move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer"
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer"
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer"
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer"
+                }
             }
         },
         rainbow = {
