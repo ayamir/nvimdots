fe2684e5785841d29fd04e8e370c2132d42f3825
ayamir <lgt986452565@gmail.com>
2021-08-22 12:26:29 +0800

    [plugin] update gitsigns config.

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -201,8 +201,7 @@ function config.gitsigns()
         },
         watch_index = {interval = 1000, follow_files = true},
         current_line_blame = true,
-        current_line_blame_delay = 1000,
-        current_line_blame_position = 'eol',
+        current_line_blame_opts = {delay = 1000, virtual_text_pos = 'eol'},
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
