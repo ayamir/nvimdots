7290695a75fdef6527c28581481843e277ac6fe2
ayamir <lgt986452565@gmail.com>
2021-08-29 01:24:03 +0800

    [plugin] update autopairs.

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -133,20 +133,10 @@ function config.tabnine()
 end
 
 function config.autopairs()
-    require('nvim-autopairs').setup({
-        disable_filetype = {"TelescopePrompt"},
-        ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
-        enable_moveright = true,
-        -- add bracket pairs after quote
-        enable_afterquote = true,
-        -- check bracket in same line
-        enable_check_bracket_line = true,
-        check_ts = true
-    })
-
-    require("nvim-autopairs.completion.cmp").setup({
+    require("nvim-autopairs.completion.compe").setup({
         map_cr = true,
-        map_complete = true
+        map_complete = true,
+        auto_select = true
     })
 end
 
