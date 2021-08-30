be1b3b9d6c7b97bffbbfdde24e0328fba9533682
ayamir <lgt986452565@gmail.com>
2021-08-30 09:05:04 +0800

    [plugin] fix wilder config.

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -157,13 +157,7 @@ end
 
 function config.wilder()
     vim.cmd [[
-call wilder#enable_cmdline_enter()
-set wildcharm=<Tab>
-cmap <expr> <Tab> wilder#in_context() ? wilder#next() : "\<Tab>"
-cmap <expr> <S-Tab> wilder#in_context() ? wilder#previous() : "\<S-Tab>"
-
-" only / and ? are enabled by default
-call wilder#set_option('modes', ['/', '?', ':'])
+call wilder#setup({'modes': [':', '/', '?']})
     ]]
 end
 
