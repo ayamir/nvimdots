58ec21f03c109d4a0d647dc1da1b2d6c7c807e37
ayamir <lgt986452565@gmail.com>
2021-09-30 14:20:34 +0800

    fix(global): compatible with Windows11's HOME.

--- lua/core/global.lua
+++ lua/core/global.lua
@@ -1,5 +1,4 @@
 local global = {}
-local home = os.getenv("HOME")
 local os_name = vim.loop.os_uname().sysname
 
 function global:load_variables()
@@ -7,7 +6,9 @@ function global:load_variables()
     self.is_linux = os_name == 'Linux'
     self.is_windows = os_name == 'Windows'
     self.vim_path = vim.fn.stdpath('config')
-    path_sep = self.is_windows and '\\' or '/'
+    local path_sep = self.is_windows and '\\' or '/'
+    local home = self.is_windows and os.getenv("USERPROFILE") or
+                     os.getenv("HOME")
     self.cache_dir = home .. path_sep .. '.cache' .. path_sep .. 'nvim' ..
                          path_sep
     self.modules_dir = self.vim_path .. path_sep .. 'modules'
