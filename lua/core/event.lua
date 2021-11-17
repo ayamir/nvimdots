904b5f2010fe433336d54af688a96fe75683fd42
ayamir <lgt986452565@gmail.com>
2021-11-17 20:41:40 +0800

    feat: add js and ts tabstop setting.

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -73,7 +73,11 @@ function autocmd.load_autocmds()
             {"FileType", "make", "set noexpandtab shiftwidth=8 softtabstop=0"},
             {"FileType", "go,rust", "setlocal tabstop=4 shiftwidth=4"},
             {"FileType", "c,cpp", "setlocal tabstop=2 shiftwidth=2"},
-            {"FileType", "html", "setlocal tabstop=2 shiftwidth=2"},
+            {
+                "FileType",
+                "javascript,typescript,html",
+                "setlocal tabstop=2 shiftwidth=2"
+            },
             {
                 "FileType",
                 "dashboard",
