f3d1d6b64ac1a952a2e37338995d4460c388fac3
ayamir <lgt986452565@gmail.com>
2021-12-01 09:43:29 +0800

    feat: update npairs'config for lisp.

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -149,13 +149,14 @@ end
 -- end
 
 function config.autopairs()
-    require("nvim-autopairs").setup {fast_wrap = {}}
+    require("nvim-autopairs").setup {}
 
     -- If you want insert `(` after select function or method item
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
     local cmp = require("cmp")
     cmp.event:on("confirm_done",
                  cmp_autopairs.on_confirm_done({map_char = {tex = ""}}))
+    cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
 end
 
 return config
