4c014d30a281fe0f315081a1e6f9f6d360f7c7d0
Pcrab <43703639+Pcrab@users.noreply.github.com>
2021-12-11 14:59:11 +0800

    fix(core): update windows os_name

--- lua/core/global.lua
+++ lua/core/global.lua
@@ -4,7 +4,7 @@ local os_name = vim.loop.os_uname().sysname
 function global:load_variables()
     self.is_mac = os_name == "Darwin"
     self.is_linux = os_name == "Linux"
-    self.is_windows = os_name == "Windows"
+    self.is_windows = os_name == "Windows_NT"
     self.vim_path = vim.fn.stdpath("config")
     local path_sep = self.is_windows and "\\" or "/"
     local home = self.is_windows and os.getenv("USERPROFILE") or
