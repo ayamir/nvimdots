10a0e3e5eba7a5e684362f6ff60bd6fc0e9b1d82
ayamir <lgt986452565@gmail.com>
2022-01-10 14:12:43 +0800

    revert: remove efm config.

--- lua/modules/completion/efm/black.lua
+++
@@ -1,7 +0,0 @@
-local formatter = 'black'
-local command = string.format('%s --no-color -q -', formatter)
-
-return {
-  formatCommand = command,
-  formatStdin = true,
-}
--- lua/modules/completion/efm/misspell.lua
+++
@@ -1,7 +0,0 @@
-return {
-    lintCommand = "misspell",
-    lintIgnoreExitCode = true,
-    lintStdin = true,
-    lintFormats = { "%f:%l:%c: %m" },
-    lintSource = "misspell",
-}
