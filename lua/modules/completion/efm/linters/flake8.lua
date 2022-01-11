a300b28579d46f37e075ecb3e184d37c3bdec761
ayamir <lgt986452565@gmail.com>
2022-01-11 22:37:39 +0800

    pref: ignore F403,F405 for flake8.

--- lua/modules/completion/efm/linters/flake8.lua
+++ lua/modules/completion/efm/linters/flake8.lua
@@ -1,7 +1,8 @@
 return {
-    lintCommand = "flake8 --max-line-length 160 --format '%(path)s:%(row)d:%(col)d: %(code)s %(code)s %(text)s' --stdin-display-name ${INPUT} -",
-    lintStdin = true,
-    lintIgnoreExitCode = true,
+    prefix = "flake8",
+    lintSource = "flake8",
+    lintCommand = "flake8 --max-line-length 160 --extend-ignore F403,F405 --format '%(path)s:%(row)d:%(col)d: %(code)s %(code)s %(text)s' --stdin-display-name ${INPUT} -",
     lintFormats = {"%f:%l:%c: %t%n%n%n %m"},
-    lintSource = "flake8"
+    lintStdin = true,
+    lintIgnoreExitCode = true
 }
