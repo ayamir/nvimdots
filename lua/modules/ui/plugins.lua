7ed1d8363e1f04f1413e50f40bb6b8e12947ba36
ayamir <lgt986452565@gmail.com>
2022-01-17 15:36:54 +0800

    feat: add kanagawa color scheme.

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -3,6 +3,7 @@ local conf = require("modules.ui.config")
 
 ui["kyazdani42/nvim-web-devicons"] = {opt = false}
 ui["sainnhe/edge"] = {opt = false, config = conf.edge}
+ui["rebelot/kanagawa.nvim"] = {opt = false, config = conf.kanagawa}
 ui["catppuccin/nvim"] = {
     opt = false,
     as = "catppuccin",
