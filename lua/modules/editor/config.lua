e9d2aaf096949baa3b264e971059734bb95f2d0d
ayamir <lgt986452565@gmail.com>
2022-02-02 11:32:20 +0800

    feat: add c/cpp/rust dap configurations, close #74.

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -267,6 +267,40 @@ end
 function config.dap()
 	local dap = require("dap")
 
+	dap.adapters.lldb = {
+		type = "executable",
+		command = "/usr/bin/lldb-vscode",
+		name = "lldb",
+	}
+	dap.configurations.cpp = {
+		{
+			name = "Launch",
+			type = "lldb",
+			request = "launch",
+			program = function()
+				return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+			end,
+			cwd = "${workspaceFolder}",
+			stopOnEntry = false,
+			args = {},
+
+			-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+			--
+			--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+			--
+			-- Otherwise you might get the following error:
+			--
+			--    Error on launch: Failed to attach to the target process
+			--
+			-- But you should be aware of the implications:
+			-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+			runInTerminal = false,
+		},
+	}
+
+	dap.configurations.c = dap.configurations.cpp
+	dap.configurations.rust = dap.configurations.cpp
+
 	dap.adapters.go = function(callback, config)
 		local stdout = vim.loop.new_pipe(false)
 		local handle
