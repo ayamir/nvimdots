e35419b39ab12b709d4a87a1b716350b6687554a
ayamir <lgt986452565@gmail.com>
2022-02-25 10:48:33 +0800

    chore: remove redundant settings for formatting.

--- lua/modules/completion/formatting.lua
+++ lua/modules/completion/formatting.lua
@@ -15,32 +15,6 @@ local format_options_var = function()
 	return string.format("format_options_%s", vim.bo.filetype)
 end
 
-local format_options_prettier = {
-	tabWidth = 4,
-	singleQuote = true,
-	trailingComma = "all",
-	configPrecedence = "prefer-file",
-}
-vim.g.format_options_typescript = format_options_prettier
-vim.g.format_options_javascript = format_options_prettier
-vim.g.format_options_typescriptreact = format_options_prettier
-vim.g.format_options_javascriptreact = format_options_prettier
-vim.g.format_options_json = format_options_prettier
-vim.g.format_options_css = format_options_prettier
-vim.g.format_options_scss = format_options_prettier
-vim.g.format_options_html = format_options_prettier
-vim.g.format_options_yaml = format_options_prettier
-vim.g.format_options_yaml = {
-	tabWidth = 2,
-	singleQuote = true,
-	trailingComma = "all",
-	configPrecedence = "prefer-file",
-}
-vim.g.format_options_markdown = format_options_prettier
-vim.g.format_options_sh = {
-	tabWidth = 4,
-}
-
 M.formatToggle = function(value)
 	local var = format_disabled_var()
 	vim.g[var] = xor(value ~= nil, value, not vim.g[var])
