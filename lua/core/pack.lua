af752e30e76484ab96af5fd47a6c7495a0d7c5b6
ayamir <lgt986452565@gmail.com>
2022-05-03 11:40:54 +0800

    chore: clone packer with ssh.

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -73,7 +73,7 @@ function Packer:init_ensure_plugins()
 	local packer_dir = data_dir .. "pack/packer/opt/packer.nvim"
 	local state = uv.fs_stat(packer_dir)
 	if not state then
-		local cmd = "!git clone https://github.com/wbthomason/packer.nvim " .. packer_dir
+		local cmd = "!git clone git@github.com:wbthomason/packer.nvim.git " .. packer_dir
 		api.nvim_command(cmd)
 		uv.fs_mkdir(data_dir .. "lua", 511, function()
 			assert("make compile path dir failed")
