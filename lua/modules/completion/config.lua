0e7b169616a606ce78acbd508971e73c422e2528
ayamir <lgt986452565@gmail.com>
2022-05-07 20:24:00 +0800

    style: remove explict color defination.

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -231,19 +231,6 @@ function config.aerial()
 end
 
 function config.cmp()
-	vim.cmd([[highlight CmpItemAbbrDeprecated guifg=#D8DEE9 guibg=NONE gui=strikethrough]])
-	vim.cmd([[highlight CmpItemKindSnippet guifg=#BF616A guibg=NONE]])
-	vim.cmd([[highlight CmpItemKindUnit guifg=#D08770 guibg=NONE]])
-	vim.cmd([[highlight CmpItemKindProperty guifg=#A3BE8C guibg=NONE]])
-	vim.cmd([[highlight CmpItemKindKeyword guifg=#EBCB8B guibg=NONE]])
-	vim.cmd([[highlight CmpItemAbbrMatch guifg=#5E81AC guibg=NONE]])
-	vim.cmd([[highlight CmpItemAbbrMatchFuzzy guifg=#5E81AC guibg=NONE]])
-	vim.cmd([[highlight CmpItemKindVariable guifg=#8FBCBB guibg=NONE]])
-	vim.cmd([[highlight CmpItemKindInterface guifg=#88C0D0 guibg=NONE]])
-	vim.cmd([[highlight CmpItemKindText guifg=#81A1C1 guibg=NONE]])
-	vim.cmd([[highlight CmpItemKindFunction guifg=#B48EAD guibg=NONE]])
-	vim.cmd([[highlight CmpItemKindMethod guifg=#B48EAD guibg=NONE]])
-
 	local t = function(str)
 		return vim.api.nvim_replace_termcodes(str, true, true, true)
 	end
