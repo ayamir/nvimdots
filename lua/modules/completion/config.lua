7dc1fed4b9e90fc950c89d3e4fcdbb7c4f8f9f1c
ayamir <lgt986452565@gmail.com>
2022-05-08 11:09:21 +0800

    chore: replace nvim-lsputils with nvim-bqf.

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -383,65 +383,50 @@ function config.autopairs()
 	cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
 end
 
-function config.nvim_lsputils()
-	if vim.fn.has("nvim-0.5.1") == 1 then
-		vim.lsp.handlers["textDocument/codeAction"] = require("lsputil.codeAction").code_action_handler
-		vim.lsp.handlers["textDocument/references"] = require("lsputil.locations").references_handler
-		vim.lsp.handlers["textDocument/definition"] = require("lsputil.locations").definition_handler
-		vim.lsp.handlers["textDocument/declaration"] = require("lsputil.locations").declaration_handler
-		vim.lsp.handlers["textDocument/typeDefinition"] = require("lsputil.locations").typeDefinition_handler
-		vim.lsp.handlers["textDocument/implementation"] = require("lsputil.locations").implementation_handler
-		vim.lsp.handlers["textDocument/documentSymbol"] = require("lsputil.symbols").document_handler
-		vim.lsp.handlers["workspace/symbol"] = require("lsputil.symbols").workspace_handler
-	else
-		local bufnr = vim.api.nvim_buf_get_number(0)
+function config.bqf()
+	vim.cmd([[
+    hi BqfPreviewBorder guifg=#F2CDCD ctermfg=71
+    hi link BqfPreviewRange Search
+]])
 
-		vim.lsp.handlers["textDocument/codeAction"] = function(_, _, actions)
-			require("lsputil.codeAction").code_action_handler(nil, actions, nil, nil, nil)
-		end
-
-		vim.lsp.handlers["textDocument/references"] = function(_, _, result)
-			require("lsputil.locations").references_handler(nil, result, {
-				bufnr = bufnr,
-			}, nil)
-		end
-
-		vim.lsp.handlers["textDocument/definition"] = function(_, method, result)
-			require("lsputil.locations").definition_handler(nil, result, {
-				bufnr = bufnr,
-				method = method,
-			}, nil)
-		end
-
-		vim.lsp.handlers["textDocument/declaration"] = function(_, method, result)
-			require("lsputil.locations").declaration_handler(nil, result, {
-				bufnr = bufnr,
-				method = method,
-			}, nil)
-		end
-
-		vim.lsp.handlers["textDocument/typeDefinition"] = function(_, method, result)
-			require("lsputil.locations").typeDefinition_handler(nil, result, {
-				bufnr = bufnr,
-				method = method,
-			}, nil)
-		end
-
-		vim.lsp.handlers["textDocument/implementation"] = function(_, method, result)
-			require("lsputil.locations").implementation_handler(nil, result, {
-				bufnr = bufnr,
-				method = method,
-			}, nil)
-		end
-
-		vim.lsp.handlers["textDocument/documentSymbol"] = function(_, _, result, _, bufn)
-			require("lsputil.symbols").document_handler(nil, result, { bufnr = bufn }, nil)
-		end
-
-		vim.lsp.handlers["textDocument/symbol"] = function(_, _, result, _, bufn)
-			require("lsputil.symbols").workspace_handler(nil, result, { bufnr = bufn }, nil)
-		end
-	end
+	require("bqf").setup({
+		auto_enable = true,
+		auto_resize_height = true, -- highly recommended enable
+		preview = {
+			win_height = 12,
+			win_vheight = 12,
+			delay_syntax = 80,
+			border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+			should_preview_cb = function(bufnr, qwinid)
+				local ret = true
+				local bufname = vim.api.nvim_buf_get_name(bufnr)
+				local fsize = vim.fn.getfsize(bufname)
+				if fsize > 100 * 1024 then
+					-- skip file size greater than 100k
+					ret = false
+				elseif bufname:match("^fugitive://") then
+					-- skip fugitive buffer
+					ret = false
+				end
+				return ret
+			end,
+		},
+		-- make `drop` and `tab drop` to become preferred
+		func_map = {
+			drop = "o",
+			openc = "O",
+			split = "<C-s>",
+			tabdrop = "<C-t>",
+			tabc = "",
+			ptogglemode = "z,",
+		},
+		filter = {
+			fzf = {
+				action_for = { ["ctrl-s"] = "split", ["ctrl-t"] = "tab drop" },
+				extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+			},
+		},
+	})
 end
 
 return config
