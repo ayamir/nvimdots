3fec1bc9002cea60bc64714cee0fd6de780a2b4e
ayamir <lgt986452565@gmail.com>
2022-05-14 20:31:20 +0800

    fix(#123): readd snippets runtimepath.

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -388,11 +388,14 @@ function config.cmp()
 end
 
 function config.luasnip()
+	vim.o.runtimepath = vim.o.runtimepath .. "," .. os.getenv("HOME") .. "/.config/nvim/my-snippets/,"
 	require("luasnip").config.set_config({
 		history = true,
 		updateevents = "TextChanged,TextChangedI",
 	})
-	require("luasnip/loaders/from_vscode").load()
+	require("luasnip.loaders.from_lua").lazy_load()
+	require("luasnip.loaders.from_vscode").lazy_load()
+	require("luasnip.loaders.from_snipmate").lazy_load()
 end
 
 -- function config.tabnine()
