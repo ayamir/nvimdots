d74a2ecefcdd2849e44c050fe24aa790284117dc
ayamir <lgt986452565@gmail.com>
2022-05-21 15:07:18 +0800

    fix: update lazy load setting for ts-autotag.

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -54,7 +54,7 @@ editor["mfussenegger/nvim-ts-hint-textobject"] = {
 }
 editor["windwp/nvim-ts-autotag"] = {
 	opt = true,
-	ft = { "html", "xml" },
+	after = "nvim-treesitter",
 	config = conf.autotag,
 }
 editor["andymass/vim-matchup"] = {
