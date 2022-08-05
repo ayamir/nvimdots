cb1d756c1286909cd3b68a3a6aeaec6f6da3b433
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-08-05 17:51:49 +0800

    fix(nvim-dap & dapui): Lazyload and integrations for DAP.

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -82,14 +82,26 @@ editor["max397574/better-escape.nvim"] = {
 	event = "BufReadPost",
 	config = conf.better_escape,
 }
-editor["rcarriga/nvim-dap-ui"] = {
-	opt = true,
-	config = conf.dapui,
-}
 editor["mfussenegger/nvim-dap"] = {
 	opt = true,
-	cmd = "DapToggleBreakpoint",
+	cmd = {
+		"DapSetLogLevel",
+		"DapShowLog",
+		"DapContinue",
+		"DapToggleBreakpoint",
+		"DapToggleRepl",
+		"DapStepOver",
+		"DapStepInto",
+		"DapStepOut",
+		"DapTerminate",
+	},
+	module = "dap",
 	config = conf.dap,
+}
+editor["rcarriga/nvim-dap-ui"] = {
+	opt = true,
+	after = "nvim-dap", -- Need to call setup after dap has been initialized.
+	config = conf.dapui,
 }
 editor["tpope/vim-fugitive"] = { opt = true, cmd = { "Git", "G" } }
 editor["famiu/bufdelete.nvim"] = {
