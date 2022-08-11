d5542b91f53c605c50bcb8a0159f94aa2773f4b4
ayamir <lgt986452565@gmail.com>
2022-08-11 19:18:53 +0800

    fix: update rust-tools settings, close #180.

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -2,52 +2,61 @@ local config = {}
 
 function config.rust_tools()
 	vim.cmd([[packadd nvim-lspconfig]])
+
 	local opts = {
-		tools = {
-			-- rust-tools options
-			-- Automatically set inlay hints (type hints)
-			autoSetHints = true,
-			-- Whether to show hover actions inside the hover window
-			-- This overrides the default hover handler
-			hover_with_actions = true,
-			runnables = {
-				-- whether to use telescope for selection menu or not
-				use_telescope = true,
+		tools = { -- rust-tools options
 
-				-- rest of the opts are forwarded to telescope
-			},
-			debuggables = {
-				-- whether to use telescope for selection menu or not
-				use_telescope = true,
+			-- how to execute terminal commands
+			-- options right now: termopen / quickfix
+			executor = require("rust-tools/executors").termopen,
 
-				-- rest of the opts are forwarded to telescope
-			},
+			-- callback to execute once rust-analyzer is done initializing the workspace
+			-- The callback receives one parameter indicating the `health` of the server: "ok" | "warning" | "error"
+			on_initialized = nil,
+
+			-- automatically call RustReloadWorkspace when writing to a Cargo.toml file.
+			reload_workspace_from_cargo_toml = true,
+
 			-- These apply to the default RustSetInlayHints command
 			inlay_hints = {
+				-- automatically set inlay hints (type hints)
+				-- default: true
+				auto = true,
+
 				-- Only show inlay hints for the current line
 				only_current_line = false,
-				-- Event which triggers a refersh of the inlay hints.
-				-- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
-				-- not that this may cause  higher CPU usage.
-				-- This option is only respected when only_current_line and
-				-- autoSetHints both are true.
-				only_current_line_autocmd = "CursorHold",
-				-- wheter to show parameter hints with the inlay hints or not
+
+				-- whether to show parameter hints with the inlay hints or not
+				-- default: true
 				show_parameter_hints = true,
+
 				-- prefix for parameter hints
+				-- default: "<-"
 				parameter_hints_prefix = "<- ",
+
 				-- prefix for all the other hints (type, chaining)
-				other_hints_prefix = " » ",
-				-- whether to align to the length of the longest line in the file
+				-- default: "=>"
+				other_hints_prefix = "=> ",
+
+				-- whether to align to the lenght of the longest line in the file
 				max_len_align = false,
+
 				-- padding from the left if max_len_align is true
 				max_len_align_padding = 1,
+
 				-- whether to align to the extreme right or not
 				right_align = false,
+
 				-- padding from the right if right_align is true
 				right_align_padding = 7,
+
+				-- The color of the hints
+				highlight = "Comment",
 			},
+
+			-- options same as lsp hover / vim.lsp.util.open_floating_preview()
 			hover_actions = {
+
 				-- the border that is used for the hover window
 				-- see vim.api.nvim_open_win()
 				border = {
@@ -60,14 +69,107 @@ function config.rust_tools()
 					{ "╰", "FloatBorder" },
 					{ "│", "FloatBorder" },
 				},
+
 				-- whether the hover action window gets automatically focused
+				-- default: false
 				auto_focus = false,
 			},
+
+			-- settings for showing the crate graph based on graphviz and the dot
+			-- command
+			crate_graph = {
+				-- Backend used for displaying the graph
+				-- see: https://graphviz.org/docs/outputs/
+				-- default: x11
+				backend = "x11",
+				-- where to store the output, nil for no output stored (relative
+				-- path from pwd)
+				-- default: nil
+				output = nil,
+				-- true for all crates.io and external crates, false only the local
+				-- crates
+				-- default: true
+				full = true,
+
+				-- List of backends found on: https://graphviz.org/docs/outputs/
+				-- Is used for input validation and autocompletion
+				-- Last updated: 2021-08-26
+				enabled_graphviz_backends = {
+					"bmp",
+					"cgimage",
+					"canon",
+					"dot",
+					"gv",
+					"xdot",
+					"xdot1.2",
+					"xdot1.4",
+					"eps",
+					"exr",
+					"fig",
+					"gd",
+					"gd2",
+					"gif",
+					"gtk",
+					"ico",
+					"cmap",
+					"ismap",
+					"imap",
+					"cmapx",
+					"imap_np",
+					"cmapx_np",
+					"jpg",
+					"jpeg",
+					"jpe",
+					"jp2",
+					"json",
+					"json0",
+					"dot_json",
+					"xdot_json",
+					"pdf",
+					"pic",
+					"pct",
+					"pict",
+					"plain",
+					"plain-ext",
+					"png",
+					"pov",
+					"ps",
+					"ps2",
+					"psd",
+					"sgi",
+					"svg",
+					"svgz",
+					"tga",
+					"tiff",
+					"tif",
+					"tk",
+					"vml",
+					"vmlz",
+					"wbmp",
+					"webp",
+					"xlib",
+					"x11",
+				},
+			},
 		},
+
 		-- all the opts to send to nvim-lspconfig
 		-- these override the defaults set by rust-tools.nvim
-		-- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-		server = {}, -- rust-analyer options
+		-- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
+		server = {
+			-- standalone file support
+			-- setting it to false may improve startup time
+			standalone = true,
+		}, -- rust-analyer options
+
+		-- debugging stuff
+		dap = {
+			adapter = {
+				type = "executable",
+				command = "lldb-vscode",
+				name = "rt_lldb",
+			},
+		},
 	}
 
 	require("rust-tools").setup(opts)
