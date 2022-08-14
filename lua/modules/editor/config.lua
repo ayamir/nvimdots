fcdda25ecefbe0bd1cacf704ae38c64a9469834f
ayamir <lgt986452565@gmail.com>
2022-08-14 18:21:13 +0800

    chore: replace accelerated-jk with lua version.

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -433,4 +433,16 @@ function config.better_escape()
 	})
 end
 
+function config.accelerated_jk()
+	require("accelerated-jk").setup({
+		mode = "time_driven",
+		enable_deceleration = false,
+		acceleration_motions = {},
+		acceleration_limit = 150,
+		acceleration_table = { 7, 12, 17, 21, 24, 26, 28, 30 },
+		-- when 'enable_deceleration = true', 'deceleration_table = { {200, 3}, {300, 7}, {450, 11}, {600, 15}, {750, 21}, {900, 9999} }'
+		deceleration_table = { { 150, 9999 } },
+	})
+end
+
 return config
--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -43,7 +43,7 @@ editor["andymass/vim-matchup"] = {
 	after = "nvim-treesitter",
 	config = conf.matchup,
 }
-editor["rhysd/accelerated-jk"] = { opt = true, event = "BufWinEnter" }
+editor["rainbowhxch/accelerated-jk.nvim"] = { opt = true, event = "BufWinEnter", config = conf.accelerated_jk }
 editor["hrsh7th/vim-eft"] = { opt = true, event = "BufReadPost" }
 editor["romainl/vim-cool"] = {
 	opt = true,
