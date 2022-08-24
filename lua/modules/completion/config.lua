9691f248df779d0afef80d5fa85e8381d5b71ea0
jczhang02 <jczhang@live.it>
2022-08-24 10:09:30 +0800

    fix: add and change luasnips option <delete_check_events> Luasnips snippets dynamic node can be triggered after undo operation Related to https://github.com/L3MON4D3/LuaSnip/issues/238. Maybe this issue can be reproduced by contributors.

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -204,6 +204,7 @@ function config.luasnip()
 	require("luasnip").config.set_config({
 		history = true,
 		updateevents = "TextChanged,TextChangedI",
+		delete_check_events = "TextChanged,InsertLeave",
 	})
 	require("luasnip.loaders.from_lua").lazy_load()
 	require("luasnip.loaders.from_vscode").lazy_load()
