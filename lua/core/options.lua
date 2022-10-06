7b177efb390b035260a81d70cdeb07c866f2d397
Charles Chiu <charleschiu2012@gmail.com>
2022-10-06 12:51:39 +0800

    clean up

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -102,12 +102,6 @@ local function load_options()
 	}
 
 	if global.is_mac then
-		vim.g.clipboard = {
-			name = "macOS-clipboard",
-			copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
-			paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
-			cache_enabled = 0,
-		}
 		vim.g.python_host_prog = "/usr/bin/python"
 		vim.g.python3_host_prog = "/usr/local/bin/python3"
 	else
