27b2285d229199f3877eca57db427ddd6a2fc2f1
ayamir <lgt986452565@gmail.com>
2022-10-08 13:38:43 +0800

    fix: remove rust.vim, attach lsp_signature to rust-analyzer.

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -2,6 +2,7 @@ local config = {}
 
 function config.rust_tools()
 	vim.api.nvim_command([[packadd nvim-lspconfig]])
+	vim.api.nvim_command([[packadd lsp_signature.nvim]])
 
 	local opts = {
 		tools = { -- rust-tools options
@@ -12,7 +13,17 @@ function config.rust_tools()
 
 			-- callback to execute once rust-analyzer is done initializing the workspace
 			-- The callback receives one parameter indicating the `health` of the server: "ok" | "warning" | "error"
-			on_initialized = nil,
+			on_initialized = function()
+				require("lsp_signature").on_attach({
+					bind = true,
+					use_lspsaga = false,
+					floating_window = true,
+					fix_pos = true,
+					hint_enable = true,
+					hi_parameter = "Search",
+					handler_opts = { "double" },
+				})
+			end,
 
 			-- automatically call RustReloadWorkspace when writing to a Cargo.toml file.
 			reload_workspace_from_cargo_toml = true,
