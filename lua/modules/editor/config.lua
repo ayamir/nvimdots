62d51af2fe377e283f12e04164fe6d450b0ae919
Charles Chiu <charleschiu2012@gmail.com>
2022-10-10 04:02:05 +0800

    revert

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -71,8 +71,10 @@ function config.nvim_treesitter()
 		p.install_info.url = p.install_info.url:gsub("https://github.com/", "git@github.com:")
 	end
 	
-	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-	parser_config.bash.filetype_to_parsername = { "sh", "zsh" }
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.bash.filetype_to_parsername = {
+            "sh", --[["zsh"--]]
+        }
 end
 
 function config.illuminate()
