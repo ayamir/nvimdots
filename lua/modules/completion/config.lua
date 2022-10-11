65c172e54b4b16c8e3fe8d55a1a586c23520bd9f
ayamir <lgt986452565@gmail.com>
2022-10-11 19:35:31 +0800

    fix: use lspsaga example config, show symbols for rust files.

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -78,6 +78,14 @@ function config.lspsaga()
 		},
 		symbol_in_winbar = {
 			enable = true,
+			in_custom = false,
+			separator = "  ",
+			show_file = false,
+			-- define how to customize filename, eg: %:., %
+			-- if not set, use default value `%:t`
+			-- more information see `vim.fn.expand` or `expand`
+			-- ## only valid after set `show_file = true`
+			file_formatter = "",
 			click_support = function(node, clicks, button, modifiers)
 				-- To see all avaiable details: vim.pretty_print(node)
 				local st = node.range.start
