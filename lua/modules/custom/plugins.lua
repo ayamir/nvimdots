7bd5dc898899d3593311902c5a3ea374ee22db1f
ayamir <lgt986452565@gmail.com>
2022-10-14 21:22:10 +0800

    feat: add user custom folder and example.

---
+++ lua/modules/custom/plugins.lua
@@ -0,0 +1,11 @@
+local custom = {}
+local conf = require("modules.custom.config")
+
+-- custom["folke/todo-comments.nvim"] = {
+-- 	-- TODO
+-- 	opt = true,
+-- 	event = "BufRead",
+-- 	config = conf.todo,
+-- }
+
+return custom
