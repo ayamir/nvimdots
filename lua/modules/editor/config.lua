722d8aabc3fe96e0a5281ce582d90482296218cd
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-10-14 19:10:57 +0800

    feat(ts-help): Enable treesitter highlight on help pages.

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -27,7 +27,7 @@ function config.nvim_treesitter()
 		},
 		highlight = {
 			enable = true,
-			disable = { "vim", "help" },
+			disable = { "vim" },
 			additional_vim_regex_highlighting = false,
 		},
 		textobjects = {
