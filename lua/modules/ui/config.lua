2958ed43b32d78554b92fd645bcc685ba20bc5fe
ayamir <lgt986452565@gmail.com>
2022-10-15 09:30:42 +0800

    refactor: remove unused config.

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -558,28 +558,6 @@ function config.lualine()
 	})
 end
 
-function config.nvim_gps()
-	require("nvim-gps").setup({
-		icons = {
-			["class-name"] = " ", -- Classes and class-like objects
-			["function-name"] = " ", -- Functions
-			["method-name"] = " ", -- Methods (functions inside class-like objects)
-		},
-		languages = {
-			-- You can disable any language individually here
-			["c"] = true,
-			["cpp"] = true,
-			["go"] = true,
-			["java"] = true,
-			["javascript"] = true,
-			["lua"] = true,
-			["python"] = true,
-			["rust"] = true,
-		},
-		separator = " > ",
-	})
-end
-
 function config.nvim_tree()
 	require("nvim-tree").setup({
 		create_in_closed_folder = false,
