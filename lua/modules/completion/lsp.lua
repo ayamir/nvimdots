dc7ce68719cbc9d8e64dd6598a790562cae7b598
Charles Chiu <charleschiu2012@gmail.com>
2022-10-15 23:01:10 +0800

    chore(lsp): Migrate to `default_capabilities`. (#286)

--- lua/modules/completion/lsp.lua
+++ lua/modules/completion/lsp.lua
@@ -21,7 +21,7 @@ mason_lsp.setup({
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local function custom_attach(client, bufnr)
 	require("lsp_signature").on_attach({
