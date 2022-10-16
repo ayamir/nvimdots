49ad88106df8a1ae54cbc2b8592d5d2f94749301
ayamir <lgt986452565@gmail.com>
2022-10-16 11:29:47 +0800

    perf: enhance code readability.

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -3,7 +3,7 @@ local home = os.getenv("HOME")
 
 settings["use_ssh"] = true
 settings["format_disabled_dirs"] = {
-	home .. "/.config/nvim",
+	home .. "/format_disabled_dir_under_home",
 }
 
 return settings
