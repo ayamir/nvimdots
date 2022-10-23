82451af2e43fb216b8a4933b9994e37a2260ddd3
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-10-23 20:19:33 +0800

    feat(catppuccin): Support function-style hl overwriting. * + Cleanup.

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -123,51 +123,8 @@ function config.nord()
 end
 
 function config.catppuccin()
-	local function get_modified_palette()
-		-- We need to explicitly declare our new color.
-		-- (Because colors haven't been set yet when we pass them to the setup function.)
-
-		local cp = require("catppuccin.palettes").get_palette() -- Get the palette.
-		cp.none = "NONE" -- Special setting for complete transparent fg/bg.
-
-		if vim.g.catppuccin_flavour == "mocha" then -- We only modify the "mocha" palette.
-			cp.rosewater = "#F5E0DC"
-			cp.flamingo = "#F2CDCD"
-			cp.mauve = "#DDB6F2"
-			cp.pink = "#F5C2E7"
-			cp.red = "#F28FAD"
-			cp.maroon = "#E8A2AF"
-			cp.peach = "#F8BD96"
-			cp.yellow = "#FAE3B0"
-			cp.green = "#ABE9B3"
-			cp.blue = "#96CDFB"
-			cp.sky = "#89DCEB"
-			cp.teal = "#B5E8E0"
-			cp.lavender = "#C9CBFF"
-
-			cp.text = "#D9E0EE"
-			cp.subtext1 = "#BAC2DE"
-			cp.subtext0 = "#A6ADC8"
-			cp.overlay2 = "#C3BAC6"
-			cp.overlay1 = "#988BA2"
-			cp.overlay0 = "#6E6C7E"
-			cp.surface2 = "#6E6C7E"
-			cp.surface1 = "#575268"
-			cp.surface0 = "#302D41"
-
-			cp.base = "#1E1E2E"
-			cp.mantle = "#1A1826"
-			cp.crust = "#161320"
-		end
-
-		return cp
-	end
-
-	vim.g.catppuccin_flavour = "mocha" -- Can be one of: latte, frappe, macchiato, mocha
-	local cp = get_modified_palette()
-
 	require("catppuccin").setup({
-		flavour = vim.g.catppuccin_flavour,
+		flavour = "mocha", -- Can be one of: latte, frappe, macchiato, mocha
 		background = { light = "latte", dark = "mocha" },
 		dim_inactive = {
 			enabled = false,
@@ -279,113 +236,115 @@ function config.catppuccin()
 			},
 		},
 		highlight_overrides = {
-			mocha = {
-				-- For base configs.
-				CursorLineNr = { fg = cp.green },
-				Search = { bg = cp.surface1, fg = cp.pink, style = { "bold" } },
-				IncSearch = { bg = cp.pink, fg = cp.surface1 },
+			mocha = function(cp)
+				return {
+					-- For base configs.
+					CursorLineNr = { fg = cp.green },
+					Search = { bg = cp.surface1, fg = cp.pink, style = { "bold" } },
+					IncSearch = { bg = cp.pink, fg = cp.surface1 },
 
-				-- For native lsp configs.
-				DiagnosticVirtualTextError = { bg = cp.none },
-				DiagnosticVirtualTextWarn = { bg = cp.none },
-				DiagnosticVirtualTextInfo = { bg = cp.none },
-				DiagnosticVirtualTextHint = { fg = cp.rosewater, bg = cp.none },
+					-- For native lsp configs.
+					DiagnosticVirtualTextError = { bg = cp.none },
+					DiagnosticVirtualTextWarn = { bg = cp.none },
+					DiagnosticVirtualTextInfo = { bg = cp.none },
+					DiagnosticVirtualTextHint = { fg = cp.rosewater, bg = cp.none },
 
-				DiagnosticHint = { fg = cp.rosewater },
-				LspDiagnosticsDefaultHint = { fg = cp.rosewater },
-				LspDiagnosticsHint = { fg = cp.rosewater },
-				LspDiagnosticsVirtualTextHint = { fg = cp.rosewater },
-				LspDiagnosticsUnderlineHint = { sp = cp.rosewater },
+					DiagnosticHint = { fg = cp.rosewater },
+					LspDiagnosticsDefaultHint = { fg = cp.rosewater },
+					LspDiagnosticsHint = { fg = cp.rosewater },
+					LspDiagnosticsVirtualTextHint = { fg = cp.rosewater },
+					LspDiagnosticsUnderlineHint = { sp = cp.rosewater },
 
-				-- For fidget.
-				FidgetTask = { bg = cp.none, fg = cp.surface2 },
-				FidgetTitle = { fg = cp.blue, style = { "bold" } },
+					-- For fidget.
+					FidgetTask = { bg = cp.none, fg = cp.surface2 },
+					FidgetTitle = { fg = cp.blue, style = { "bold" } },
 
-				-- For treesitter.
-				["@field"] = { fg = cp.rosewater },
-				["@property"] = { fg = cp.yellow },
+					-- For treesitter.
+					["@field"] = { fg = cp.rosewater },
+					["@property"] = { fg = cp.yellow },
 
-				["@include"] = { fg = cp.teal },
-				["@operator"] = { fg = cp.sky },
-				["@keyword.operator"] = { fg = cp.sky },
-				["@punctuation.special"] = { fg = cp.maroon },
+					["@include"] = { fg = cp.teal },
+					["@operator"] = { fg = cp.sky },
+					["@keyword.operator"] = { fg = cp.sky },
+					["@punctuation.special"] = { fg = cp.maroon },
 
-				-- ["@float"] = { fg = cp.peach },
-				-- ["@number"] = { fg = cp.peach },
-				-- ["@boolean"] = { fg = cp.peach },
+					-- ["@float"] = { fg = cp.peach },
+					-- ["@number"] = { fg = cp.peach },
+					-- ["@boolean"] = { fg = cp.peach },
 
-				["@constructor"] = { fg = cp.lavender },
-				-- ["@constant"] = { fg = cp.peach },
-				-- ["@conditional"] = { fg = cp.mauve },
-				-- ["@repeat"] = { fg = cp.mauve },
-				["@exception"] = { fg = cp.peach },
+					["@constructor"] = { fg = cp.lavender },
+					-- ["@constant"] = { fg = cp.peach },
+					-- ["@conditional"] = { fg = cp.mauve },
+					-- ["@repeat"] = { fg = cp.mauve },
+					["@exception"] = { fg = cp.peach },
 
-				["@constant.builtin"] = { fg = cp.lavender },
-				-- ["@function.builtin"] = { fg = cp.peach, style = { "italic" } },
-				-- ["@type.builtin"] = { fg = cp.yellow, style = { "italic" } },
-				["@variable.builtin"] = { fg = cp.red, style = { "italic" } },
+					["@constant.builtin"] = { fg = cp.lavender },
+					-- ["@function.builtin"] = { fg = cp.peach, style = { "italic" } },
+					-- ["@type.builtin"] = { fg = cp.yellow, style = { "italic" } },
+					["@variable.builtin"] = { fg = cp.red, style = { "italic" } },
 
-				-- ["@function"] = { fg = cp.blue },
-				["@function.macro"] = { fg = cp.red, style = {} },
-				["@parameter"] = { fg = cp.rosewater },
-				["@keyword.function"] = { fg = cp.maroon },
-				["@keyword"] = { fg = cp.red },
-				["@keyword.return"] = { fg = cp.pink, style = {} },
+					-- ["@function"] = { fg = cp.blue },
+					["@function.macro"] = { fg = cp.red, style = {} },
+					["@parameter"] = { fg = cp.rosewater },
+					["@keyword.function"] = { fg = cp.maroon },
+					["@keyword"] = { fg = cp.red },
+					["@keyword.return"] = { fg = cp.pink, style = {} },
 
-				-- ["@text.note"] = { fg = cp.base, bg = cp.blue },
-				-- ["@text.warning"] = { fg = cp.base, bg = cp.yellow },
-				-- ["@text.danger"] = { fg = cp.base, bg = cp.red },
-				-- ["@constant.macro"] = { fg = cp.mauve },
+					-- ["@text.note"] = { fg = cp.base, bg = cp.blue },
+					-- ["@text.warning"] = { fg = cp.base, bg = cp.yellow },
+					-- ["@text.danger"] = { fg = cp.base, bg = cp.red },
+					-- ["@constant.macro"] = { fg = cp.mauve },
 
-				-- ["@label"] = { fg = cp.blue },
-				["@method"] = { style = { "italic" } },
-				["@namespace"] = { fg = cp.rosewater, style = {} },
+					-- ["@label"] = { fg = cp.blue },
+					["@method"] = { style = { "italic" } },
+					["@namespace"] = { fg = cp.rosewater, style = {} },
 
-				["@punctuation.delimiter"] = { fg = cp.teal },
-				["@punctuation.bracket"] = { fg = cp.overlay2 },
-				-- ["@string"] = { fg = cp.green },
-				-- ["@string.regex"] = { fg = cp.peach },
-				-- ["@type"] = { fg = cp.yellow },
-				["@variable"] = { fg = cp.text },
-				["@tag.attribute"] = { fg = cp.mauve, style = { "italic" } },
-				["@tag"] = { fg = cp.peach },
-				["@tag.delimiter"] = { fg = cp.maroon },
-				["@text"] = { fg = cp.text },
+					["@punctuation.delimiter"] = { fg = cp.teal },
+					["@punctuation.bracket"] = { fg = cp.overlay2 },
+					-- ["@string"] = { fg = cp.green },
+					-- ["@string.regex"] = { fg = cp.peach },
+					-- ["@type"] = { fg = cp.yellow },
+					["@variable"] = { fg = cp.text },
+					["@tag.attribute"] = { fg = cp.mauve, style = { "italic" } },
+					["@tag"] = { fg = cp.peach },
+					["@tag.delimiter"] = { fg = cp.maroon },
+					["@text"] = { fg = cp.text },
 
-				-- ["@text.uri"] = { fg = cp.rosewater, style = { "italic", "underline" } },
-				-- ["@text.literal"] = { fg = cp.teal, style = { "italic" } },
-				-- ["@text.reference"] = { fg = cp.lavender, style = { "bold" } },
-				-- ["@text.title"] = { fg = cp.blue, style = { "bold" } },
-				-- ["@text.emphasis"] = { fg = cp.maroon, style = { "italic" } },
-				-- ["@text.strong"] = { fg = cp.maroon, style = { "bold" } },
-				-- ["@string.escape"] = { fg = cp.pink },
+					-- ["@text.uri"] = { fg = cp.rosewater, style = { "italic", "underline" } },
+					-- ["@text.literal"] = { fg = cp.teal, style = { "italic" } },
+					-- ["@text.reference"] = { fg = cp.lavender, style = { "bold" } },
+					-- ["@text.title"] = { fg = cp.blue, style = { "bold" } },
+					-- ["@text.emphasis"] = { fg = cp.maroon, style = { "italic" } },
+					-- ["@text.strong"] = { fg = cp.maroon, style = { "bold" } },
+					-- ["@string.escape"] = { fg = cp.pink },
 
-				-- ["@property.toml"] = { fg = cp.blue },
-				-- ["@field.yaml"] = { fg = cp.blue },
+					-- ["@property.toml"] = { fg = cp.blue },
+					-- ["@field.yaml"] = { fg = cp.blue },
 
-				-- ["@label.json"] = { fg = cp.blue },
+					-- ["@label.json"] = { fg = cp.blue },
 
-				["@function.builtin.bash"] = { fg = cp.red, style = { "italic" } },
-				["@parameter.bash"] = { fg = cp.yellow, style = { "italic" } },
+					["@function.builtin.bash"] = { fg = cp.red, style = { "italic" } },
+					["@parameter.bash"] = { fg = cp.yellow, style = { "italic" } },
 
-				["@field.lua"] = { fg = cp.lavender },
-				["@constructor.lua"] = { fg = cp.flamingo },
+					["@field.lua"] = { fg = cp.lavender },
+					["@constructor.lua"] = { fg = cp.flamingo },
 
-				["@constant.java"] = { fg = cp.teal },
+					["@constant.java"] = { fg = cp.teal },
 
-				["@property.typescript"] = { fg = cp.lavender, style = { "italic" } },
-				-- ["@constructor.typescript"] = { fg = cp.lavender },
+					["@property.typescript"] = { fg = cp.lavender, style = { "italic" } },
+					-- ["@constructor.typescript"] = { fg = cp.lavender },
 
-				-- ["@constructor.tsx"] = { fg = cp.lavender },
-				-- ["@tag.attribute.tsx"] = { fg = cp.mauve },
+					-- ["@constructor.tsx"] = { fg = cp.lavender },
+					-- ["@tag.attribute.tsx"] = { fg = cp.mauve },
 
-				["@type.css"] = { fg = cp.lavender },
-				["@property.css"] = { fg = cp.yellow, style = { "italic" } },
+					["@type.css"] = { fg = cp.lavender },
+					["@property.css"] = { fg = cp.yellow, style = { "italic" } },
 
-				["@property.cpp"] = { fg = cp.text },
+					["@property.cpp"] = { fg = cp.text },
 
-				-- ["@symbol"] = { fg = cp.flamingo },
-			},
+					-- ["@symbol"] = { fg = cp.flamingo },
+				}
+			end,
 		},
 	})
 end
