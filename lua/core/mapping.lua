092186e5a30ad9162222453a14607fb9ebec93e0
ayamir <lgt986452565@gmail.com>
2022-11-11 17:27:17 +0800

    fix: remove <Tab> and <S-Tab>'s mappings, in case of <C-I>'s function overrided by fold, close #321.

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -6,8 +6,6 @@ local map_cmd = bind.map_cmd
 -- default map
 local def_map = {
 	-- Vim map
-	["n|<Tab>"] = map_cr("normal zc"):with_noremap():with_silent(),
-	["n|<S-Tab>"] = map_cr("normal zo"):with_noremap():with_silent(),
 	["n|<C-x>k"] = map_cr("bdelete"):with_noremap():with_silent(),
 	["n|<C-s>"] = map_cu("write"):with_noremap(),
 	["n|Y"] = map_cmd("y$"),
