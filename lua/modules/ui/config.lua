a001e97d8f3826a3254b824929eb6323620be678
ayamir <lgt986452565@gmail.com>
2022-12-11 14:28:19 +0800

    fix(ui): eliminate neodim's blend_color error for every colorscheme.

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -357,9 +357,11 @@ end
 
 function config.neodim()
 	vim.api.nvim_command([[packadd nvim-treesitter]])
+	local normal_background = vim.api.nvim_get_hl_by_name("Normal", true).background
+	local blend_color = normal_background ~= nil and string.format("#%06x", normal_background) or "#000000"
 	require("neodim").setup({
 		alpha = 0.45,
-		blend_color = string.format("#%06x", vim.api.nvim_get_hl_by_name("Normal", true).background),
+		blend_color = blend_color,
 		update_in_insert = {
 			enable = true,
 			delay = 100,
