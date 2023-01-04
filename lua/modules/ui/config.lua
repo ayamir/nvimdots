c65897e03dba0ba3dbdb7428dd9bdbc077b64cb6
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-01-04 11:26:42 +0800

    fix(enew): Ensure treesitter is in `rtp`

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -360,6 +360,7 @@ function config.catppuccin()
 end
 
 function config.neodim()
+	vim.api.nvim_command([[packadd nvim-treesitter]])
 	local normal_background = vim.api.nvim_get_hl_by_name("Normal", true).background
 	local blend_color = normal_background ~= nil and string.format("#%06x", normal_background) or "#000000"
 
