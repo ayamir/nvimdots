4ea4296ac84934e7dd2696d9ecb4cc70492dacda
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-01-20 19:56:48 +0800

    chore(pack & mason): Update icons

--- lua/modules/completion/lsp.lua
+++ lua/modules/completion/lsp.lua
@@ -6,9 +6,29 @@ local mason_lsp = require("mason-lspconfig")
 
 require("lspconfig.ui.windows").default_options.border = "single"
 
+local icons = {
+	ui = require("modules.ui.icons").get("ui", true),
+	misc = require("modules.ui.icons").get("misc", true),
+}
+
 mason.setup({
 	ui = {
 		border = "rounded",
+		icons = {
+			package_pending = icons.ui.Modified_alt,
+			package_installed = icons.ui.Check,
+			package_uninstalled = icons.misc.Ghost,
+		},
+		keymaps = {
+			toggle_server_expand = "<CR>",
+			install_server = "i",
+			update_server = "u",
+			check_server_version = "c",
+			update_all_servers = "U",
+			check_outdated_servers = "C",
+			uninstall_server = "X",
+			cancel_installation = "<C-c>",
+		},
 	},
 })
 mason_lsp.setup({
