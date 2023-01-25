a4cd0fae80fa8eb42781b5d20f65a870c29d6a5c
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-01-26 00:22:38 +0800

    chore(tabout): Minor fixes

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -518,9 +518,7 @@ function config.tabout()
 		backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
 		act_as_tab = true, -- shift content if tab out is not possible
 		act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-		default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-		default_shift_tab = "<C-d>", -- reverse shift default action,
-		enable_backwards = true, -- well ...
+		enable_backwards = true,
 		completion = true, -- if the tabkey is used in a completion pum
 		tabouts = {
 			{ open = "'", close = "'" },
@@ -530,7 +528,7 @@ function config.tabout()
 			{ open = "[", close = "]" },
 			{ open = "{", close = "}" },
 		},
-		ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+		ignore_beginning = true, -- if the cursor is at the beginning of a filled element it will rather tab out than shift the content
 		exclude = {}, -- tabout will ignore these filetypes
 	})
 end
