7d0b3c024820f4de337f8fa8a6eaebfceb8fa661
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-01-27 12:41:42 +0800

    fix(tabout): Unset default keymap

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -513,8 +513,8 @@ end
 
 function config.tabout()
 	require("tabout").setup({
-		tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
-		backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
+		tabkey = "", -- key to trigger tabout, set to an empty string to disable
+		backwards_tabkey = "", -- key to trigger backwards tabout, set to an empty string to disable
 		act_as_tab = true, -- shift content if tab out is not possible
 		act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
 		enable_backwards = true,
