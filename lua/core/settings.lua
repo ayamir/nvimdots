fee14c3f949e05143c36b4bc4217e9af269b8e60
ayamir <lgt986452565@gmail.com>
2023-01-28 22:13:25 +0800

    perf: add more comments for each setting.

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,15 +1,18 @@
 local settings = {}
 local home = require("core.global").home
 
+-- Set it to false if you want to use https to update plugins and treesitter parsers.
 settings["use_ssh"] = true
 
+-- Set it to false if there are no need to format on save.
 settings["format_on_save"] = true
 
+-- Set the format disabled directories here, files under these dirs won't be formatted on save.
 settings["format_disabled_dirs"] = {
 	home .. "/format_disabled_dir_under_home",
 }
 
--- Disable features that could affect performance when loading large files
+-- Set it to false if you don't use nvim to open big files.
 settings["load_big_files_faster"] = true
 
 ---Change the colors of the global palette here.
