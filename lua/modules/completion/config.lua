f1c97f14b2af347680196f5083fc5f5a1597a842
ayamir <lgt986452565@gmail.com>
2023-01-29 19:20:08 +0800

    style: remove unused code block.

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -177,11 +177,6 @@ function config.cmp()
 		return vim.api.nvim_replace_termcodes(str, true, true, true)
 	end
 
-	local has_words_before = function()
-		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-	end
-
 	local border = function(hl)
 		return {
 			{ "╭", hl },
