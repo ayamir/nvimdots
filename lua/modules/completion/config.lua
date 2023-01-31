31667be8caedb5494ff6de31be422674124d8b7c
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-01-31 10:36:02 +0800

    chore(lspsaga): Show code action with lsp diagnostics

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -70,7 +70,7 @@ function config.lspsaga()
 			virtual_text = false,
 		},
 		diagnostic = {
-			show_code_action = false,
+			show_code_action = true,
 			show_source = true,
 			jump_num_shortcut = true,
 			keys = {
