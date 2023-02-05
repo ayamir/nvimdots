501927194a93cca989c3ebf0adbbb8041e51494f
ayamir <lgt986452565@gmail.com>
2023-02-05 19:28:11 +0800

    feat(keymap): make cursor can move in and out of terminal.

--- lua/keymap/config.lua
+++ lua/keymap/config.lua
@@ -34,3 +34,15 @@ _G.toggle_lazygit = function()
 	end
 	_lazygit:toggle()
 end
+
+_G.set_terminal_keymaps = function()
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+end
+
+vim.api.nvim_command([[autocmd! TermOpen term://* lua set_terminal_keymaps()]])
