62306e24dc4fbd175b60be32209bbd57712daa45
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-02-06 16:12:53 +0800

    chore: Add line break

--- init.lua
+++ init.lua
@@ -5,7 +5,8 @@ if not vim.g.vscode then
 	vim.schedule(function()
 		vim.notify_once(
 			[[
-We've released version v2.0.0! This is a backward-incompatible release. See the release notes for instructions on how to migrate your configs.
+We've released version v2.0.0!
+This is a backward-incompatible release. See the release notes for instructions on how to migrate your configs.
 
 Visit https://github.com/ayamir/nvimdots/releases to see the release notes.
 
