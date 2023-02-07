dfe262b4d5bfd640ff2db5e14e215ce6ef190d14
ayamir <lgt986452565@gmail.com>
2023-02-07 12:30:43 +0800

    feat(keymap): define comment mappings with descriptions.

--- lua/modules/configs/editor/comment.lua
+++ lua/modules/configs/editor/comment.lua
@@ -1,45 +1,44 @@
 return function()
 	require("Comment").setup({
-		---Add a space b/w comment and the line
+		-- Add a space b/w comment and the line
 		padding = true,
-		---Whether the cursor should stay at its position
+		-- Whether the cursor should stay at its position
 		sticky = true,
-		---Lines to be ignored while (un)comment
-		ignore = nil,
-		---LHS of toggle mappings in NORMAL mode
+		-- Lines to be ignored while (un)comment
+		ignore = "^$",
+		-- LHS of toggle mappings in NORMAL mode
 		toggler = {
-			---Line-comment toggle keymap
+			-- Line-comment toggle keymap
 			line = "gcc",
-			---Block-comment toggle keymap
+			-- Block-comment toggle keymap
 			block = "gbc",
 		},
-		---LHS of operator-pending mappings in NORMAL and VISUAL mode
+		-- LHS of operator-pending mappings in NORMAL and VISUAL mode
 		opleader = {
-			---Line-comment keymap
+			-- Line-comment keymap
 			line = "gc",
-			---Block-comment keymap
+			-- Block-comment keymap
 			block = "gb",
 		},
-		---LHS of extra mappings
+		-- LHS of extra mappings
 		extra = {
-			---Add comment on the line above
+			-- Add comment on the line above
 			above = "gcO",
-			---Add comment on the line below
+			-- Add comment on the line below
 			below = "gco",
-			---Add comment at the end of line
+			-- Add comment at the end of line
 			eol = "gcA",
 		},
-		---Enable keybindings
-		---NOTE: If given `false` then the plugin won't create any mappings
+		-- We defined mappings in `lua/keymap/init.lua` with description so disable them here.
 		mappings = {
-			---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-			basic = true,
-			---Extra mapping; `gco`, `gcO`, `gcA`
-			extra = true,
+			-- Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+			basic = false,
+			-- Extra mapping; `gco`, `gcO`, `gcA`
+			extra = false,
 		},
-		---Function to call before (un)comment
+		-- Function to call before (un)comment
 		pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-		---Function to call after (un)comment
+		-- Function to call after (un)comment
 		post_hook = nil,
 	})
 end
