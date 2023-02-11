98ce72f7a5cea6bfca4a48fc551a8c40cbf53939
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-02-11 01:48:17 -0800

    fix: api.nvim_setkeymap will cause buffer to fail
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -34,6 +34,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		"qf",
 		"help",
 		"man",
+		"notify",
 		"nofile",
 		"lspinfo",
 		"terminal",
@@ -46,7 +47,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	},
 	callback = function(event)
 		vim.bo[event.buf].buflisted = false
-		vim.api.nvim_set_keymap("n", "q", "<CMD>close<CR>", { buffer = event.buf, silent = true })
+		vim.keymap.set("n", "q", "<CMD>close<CR>", { buffer = event.buf, silent = true })
 	end,
 })
 
