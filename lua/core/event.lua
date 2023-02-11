58b483218897b46d79e3da7e9044b9fd26ddc130
Charles Chiu <charleschiu2012@gmail.com>
2023-02-11 17:10:36 +0800

    chore: use `vim.api.nvim_set_keymap` instead

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -47,7 +47,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	},
 	callback = function(event)
 		vim.bo[event.buf].buflisted = false
-		vim.keymap.set("n", "q", "<CMD>close<CR>", { buffer = event.buf, silent = true })
+		vim.api.nvim_set_keymap("n", "q", "<CMD>close<CR>", { buffer = event.buf, silent = true })
 	end,
 })
 
