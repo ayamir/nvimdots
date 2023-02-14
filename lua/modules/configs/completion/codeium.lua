2c0185df1843116be2e0a34a8809e8cedf872854
CharlesChiuGit <charleschiu2012@gmail.com>
2023-02-14 15:34:30 +0800

    sync: add/replace plugins ( #514 )

---
+++ lua/modules/configs/completion/codeium.lua
@@ -0,0 +1,3 @@
+return function()
+	require("codeium").setup({})
+end
---
+++ lua/modules/configs/ui/paint.lua
@@ -0,0 +1,20 @@
+return function()
+	require("paint").setup({
+		---type PaintHighlight[]
+		highlights = {
+			{
+				-- filter can be a table of buffer options that should match,
+				-- or a function called with buf as param that should return true.
+				-- The example below will paint @something in comments with Constant
+				filter = { filetype = "lua" },
+				pattern = "%s*%-%-%-%s*(@%w+)",
+				hl = "Constant",
+			},
+			{
+				filter = { filetype = "python" },
+				pattern = "%s*(%w+:)",
+				hl = "Constant",
+			},
+		},
+	})
+end
