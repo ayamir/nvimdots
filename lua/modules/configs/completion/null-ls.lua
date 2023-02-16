2ff5d9655f8fbede7e6745e5765c575b70c058c4
CharlesChiuGit <charleschiu2012@gmail.com>
2023-02-17 00:56:00 +0800

    refactor: move `lsp` and `null-ls` sources to `core/settings.lua`

--- lua/modules/configs/completion/null-ls.lua
+++ lua/modules/configs/completion/null-ls.lua
@@ -1,38 +1,35 @@
 return function()
-	local formatting = require("completion.formatting")
+	local btn = require("null-ls").builtins
 
-	local null_ls = require("null-ls")
-	local btn = null_ls.builtins -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins
+	require("mason-null-ls").setup({
+		ensure_installed = require("core.settings").null_ls,
+		automatic_installation = true,
+		automatic_setup = true,
+	})
 
-	local sources = {
-		-- formatting
-		btn.formatting.black.with({ extra_args = { "--fast" } }),
-		btn.formatting.clang_format,
-		btn.formatting.eslint_d,
-		btn.formatting.jq,
-		btn.formatting.markdownlint,
-		btn.formatting.prettierd,
-		btn.formatting.rustfmt,
-		btn.formatting.shfmt,
-		btn.formatting.stylua,
-
-		-- diagnostics
-		btn.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
-		btn.diagnostics.markdownlint.with({ extra_args = { "--disable MD033" } }),
-	}
-
-	null_ls.setup({
+	require("null-ls").setup({
 		debug = false,
 		update_in_insert = false,
 		diagnostics_format = "[#{c}] #{m} (#{s})",
-		sources = sources,
+		sources = {
+			-- formatting
+			btn.formatting.black.with({ extra_args = { "--fast" } }),
+			btn.formatting.clang_format,
+			btn.formatting.eslint_d,
+			btn.formatting.jq,
+			btn.formatting.markdownlint,
+			btn.formatting.prettierd,
+			btn.formatting.rustfmt,
+			btn.formatting.shfmt,
+			btn.formatting.stylua,
+
+			-- diagnostics
+			btn.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+			btn.diagnostics.markdownlint.with({ extra_args = { "--disable MD033" } }),
+		},
 	})
 
-	require("mason-null-ls").setup({
-		ensure_installed = nil,
-		automatic_installation = true,
-		automatic_setup = false,
-	})
+	require("mason-null-ls").setup_handlers()
 
-	formatting.configure_format_on_save()
+	require("completion.formatting").configure_format_on_save()
 end
