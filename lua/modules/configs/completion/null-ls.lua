961f8a615559c33ff8ccfbfe3b748aaee566a792
CharlesChiuGit <charleschiu2012@gmail.com>
2023-02-17 08:51:19 +0800

    chore(mason-null-ls): use auto-`setup_handlers` for default config

--- lua/modules/configs/completion/null-ls.lua
+++ lua/modules/configs/completion/null-ls.lua
@@ -11,17 +11,12 @@ return function()
 		debug = false,
 		update_in_insert = false,
 		diagnostics_format = "[#{c}] #{m} (#{s})",
+		-- NOTE: Users don't need to specify null-ls sources if using only default config.
+		-- "mason-null-ls" will auto-setup for users.
 		sources = {
 			-- formatting
 			btn.formatting.black.with({ extra_args = { "--fast" } }),
-			btn.formatting.clang_format,
-			btn.formatting.eslint_d,
-			btn.formatting.jq,
 			btn.formatting.markdownlint,
-			btn.formatting.prettierd,
-			btn.formatting.rustfmt,
-			btn.formatting.shfmt,
-			btn.formatting.stylua,
 
 			-- diagnostics
 			btn.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
