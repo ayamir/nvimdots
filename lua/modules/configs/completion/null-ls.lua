930d3ca1f686d6fd608d68492a31adf79baadfbf
CharlesChiuGit <charleschiu2012@gmail.com>
2023-02-17 09:59:49 +0800

    perf: use only `mason-null-ls` to setup sources to prevent overwrite.

--- lua/modules/configs/completion/null-ls.lua
+++ lua/modules/configs/completion/null-ls.lua
@@ -1,30 +1,36 @@
 return function()
-	local btn = require("null-ls").builtins
+	local null_ls = require("null-ls")
+	local mason_null_ls = require("mason-null-ls")
+	local btnf = null_ls.builtins.formatting
+	local btnd = null_ls.builtins.diagnostics
+	local null_reg = null_ls.register
 
-	require("mason-null-ls").setup({
+	null_ls.setup({
+		debug = false,
+		update_in_insert = false,
+		diagnostics_format = "[#{c}] #{m} (#{s})",
+	})
+
+	mason_null_ls.setup({
 		ensure_installed = require("core.settings").null_ls,
 		automatic_installation = true,
 		automatic_setup = true,
 	})
 
-	require("null-ls").setup({
-		debug = false,
-		update_in_insert = false,
-		diagnostics_format = "[#{c}] #{m} (#{s})",
-		-- NOTE: Users don't need to specify null-ls sources if using only default config.
-		-- "mason-null-ls" will auto-setup for users.
-		sources = {
-			-- formatting
-			btn.formatting.black.with({ extra_args = { "--fast" } }),
-			btn.formatting.markdownlint,
-
-			-- diagnostics
-			btn.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
-			btn.diagnostics.markdownlint.with({ extra_args = { "--disable MD033" } }),
-		},
+	-- NOTE: Users don't need to specify null-ls sources if using only default config.
+	-- "mason-null-ls" will auto-setup for users.
+	mason_null_ls.setup_handlers({
+		black = function()
+			null_reg(btnf.black.with({ extra_args = { "--fast" } }))
+		end,
+		markdownlint = function()
+			null_reg(btnf.markdownlint)
+			null_reg(btnd.markdownlint.with({ extra_args = { "--disable MD033" } }))
+		end,
+		shellcheck = function()
+			null_reg(btnd.shellcheck.with({ diagnostics_format = "#{m} [#(c)]" }))
+		end,
 	})
-
-	require("mason-null-ls").setup_handlers()
 
 	require("completion.formatting").configure_format_on_save()
 end
