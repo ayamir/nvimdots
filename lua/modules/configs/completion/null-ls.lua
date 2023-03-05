3165d596c2a35120fbad0fa80827ba35ea8fd379
CharlesChiuGit <charleschiu2012@gmail.com>
2023-03-05 22:55:53 +0800

    chore(mason-null-ls): move extra settings to `mason-null-ls` to prevent overwriten

--- lua/modules/configs/completion/null-ls.lua
+++ lua/modules/configs/completion/null-ls.lua
@@ -2,39 +2,14 @@ return function()
 	local null_ls = require("null-ls")
 	local mason_null_ls = require("mason-null-ls")
 	local btns = null_ls.builtins
+	local null_reg = null_ls.register
 
-	-- Please set additional flags for the supported servers here
-	-- Don't specify any config here if you are using the default one.
-	local sources = {
-		btns.formatting.black.with({
-			extra_args = { "--fast" },
-		}),
-		btns.formatting.clang_format.with({
-			filetypes = { "c", "cpp" },
-			extra_args = require("completion.formatters.clang_format"),
-		}),
-		btns.formatting.prettier.with({
-			filetypes = {
-				"vue",
-				"typescript",
-				"javascript",
-				"typescriptreact",
-				"javascriptreact",
-				"yaml",
-				"html",
-				"css",
-				"scss",
-				"sh",
-				"markdown",
-			},
-		}),
-	}
 	null_ls.setup({
 		border = "rounded",
 		debug = false,
 		log_level = "warn",
 		update_in_insert = false,
-		sources = sources,
+		sources = nil,
 	})
 
 	mason_null_ls.setup({
@@ -42,7 +17,37 @@ return function()
 		automatic_installation = false,
 		automatic_setup = true,
 	})
-	require("mason-null-ls").setup_handlers()
+
+	-- Please set additional flags for the supported servers here
+	-- Don't specify any config here if you are using the default one.
+	mason_null_ls.setup_handlers({
+		black = function()
+			null_reg(btns.formatting.black.with({ extra_args = { "--fast" } }))
+		end,
+		clang_format = function()
+			null_reg(btns.formatting.clang_format.with({
+				filetypes = { "c", "cpp" },
+				extra_args = require("completion.formatters.clang_format"),
+			}))
+		end,
+		prettier = function()
+			null_reg(btns.formatting.prettier.with({
+				filetypes = {
+					"vue",
+					"typescript",
+					"javascript",
+					"typescriptreact",
+					"javascriptreact",
+					"yaml",
+					"html",
+					"css",
+					"scss",
+					"sh",
+					"markdown",
+				},
+			}))
+		end,
+	})
 
 	require("completion.formatting").configure_format_on_save()
 end
