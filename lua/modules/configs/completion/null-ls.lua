0981234b4b045717a258d2db81fc377022ad4497
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-03-05 22:30:10 +0800

    chore(null-ls): reorganize entries

--- lua/modules/configs/completion/null-ls.lua
+++ lua/modules/configs/completion/null-ls.lua
@@ -6,12 +6,12 @@ return function()
 	-- Please set additional flags for the supported servers here
 	-- Don't specify any config here if you are using the default one.
 	local sources = {
+		btns.formatting.black.with({
+			extra_args = { "--fast" },
+		}),
 		btns.formatting.clang_format.with({
 			filetypes = { "c", "cpp" },
 			extra_args = require("completion.formatters.clang_format"),
-		}),
-		btns.formatting.black.with({
-			extra_args = { "--fast" },
 		}),
 		btns.formatting.prettier.with({
 			filetypes = {
