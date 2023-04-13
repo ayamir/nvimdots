f9668aed9067316a3c4a2fc78c3a2a0953697ebe
CharlesChiuGit <charleschiu2012@gmail.com>
2023-04-13 16:53:55 +0800

    chore(project.nvim): update legacy code

--- lua/modules/configs/tool/project.lua
+++ lua/modules/configs/tool/project.lua
@@ -3,7 +3,7 @@ return function()
 		manual_mode = false,
 		detection_methods = { "lsp", "pattern" },
 		patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
-		ignore_lsp = { "efm", "copilot" },
+		ignore_lsp = { "null-ls", "copilot" },
 		exclude_dirs = {},
 		show_hidden = false,
 		silent_chdir = true,
