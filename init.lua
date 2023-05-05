bd81d052bb6f99f64470172257c14dd17486a313
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-05-05 20:38:00 +0800

    add release note

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
 if not vim.g.vscode then
 	require("core")
+
+	-- Release note
+	vim.schedule(function()
+		vim.notify_once(
+			[[
+We've released version v3.0.0!
+Visit https://github.com/ayamir/nvimdots/releases to see the release notes.
+If you have icons that can't be rendered correctly (e.g., 𑨩  or � ), be sure to read this!
+
+To silence this message, remove it from `init.lua` at the config's root directory.
+]],
+			vim.log.levels.WARN
+		)
+	end)
 end
