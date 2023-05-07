56538b791d3354dd6a5af257be0bb3eaf52765bd
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-05-07 12:24:48 +0800

    fix(icons)!: update icon codepoints (#447)

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -9,6 +9,10 @@ settings["use_ssh"] = true
 ---@type boolean
 settings["format_on_save"] = true
 
+-- Set it to false if diagnostics virtual text is annoying for you
+---@type boolean
+settings["diagnostics_virtual_text"] = true
+
 -- Set the format disabled directories here, files under these dirs won't be formatted on save.
 ---@type string[]
 settings["format_disabled_dirs"] = {
@@ -89,9 +93,5 @@ settings["null_ls_deps"] = {
 	"stylua",
 	"vint",
 }
-
--- Set the inline_diagnostic_virtual_text to false if they are annoying for you
----@type boolean
-settings["inline_diagnostic_virtual_text"] = true
 
 return settings
