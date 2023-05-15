8f8ee50e53466f539724281d4b46695641f42574
alo-s <anlongtu19@gmail.com>
2023-05-15 22:38:53 +0800

    refactor: update wsl clipboard for windows with recomand

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -101,16 +101,16 @@ local clipboard_config = function()
 		}
 	elseif global.is_wsl then
 		vim.g.clipboard = {
-			name = "win32yank-wsl",
-			copy = {
-				["+"] = "win32yank.exe -i --crlf",
-				["*"] = "win32yank.exe -i --crlf",
-			},
-			paste = {
-				["+"] = "win32yank.exe -o --lf",
-				["*"] = "win32yank.exe -o --lf",
-			},
-			cache_enabled = 0,
+            name = "WslClipboard",
+            copy= {
+                ['+']= 'clip.exe',
+                ['*']= 'clip.exe'
+            },
+            paste= {
+                ['+']= 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r" ""))',
+                ['*']= 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r" ""))',
+            },
+            cache_enabled= 0
 		}
 	end
 end
