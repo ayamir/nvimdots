e17000bc1f3c84a1af90f2cda757624080910957
ayamir <lgt986452565@gmail.com>
2023-05-15 22:25:13 +0800

    style: update code style.
    Co-authored-by: 冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -107,8 +107,8 @@ local clipboard_config = function()
 				["*"] = "clip.exe",
 			},
 			paste = {
-				["+"] = [[powershell.exe -noprofile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
-				["*"] = [[powershell.exe -noprofile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
+				["+"] = [[powershell.exe -NoProfile -NoLogo -NonInteractive -MTA -Command [console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
+				["*"] = [[powershell.exe -NoProfile -NoLogo -NonInteractive -MTA -Command [console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
 			},
 			cache_enabled = 0,
 		}
