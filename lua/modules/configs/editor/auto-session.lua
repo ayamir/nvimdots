e8f6829a8b453f22c1a85adac5f66ebecfe12b66
ayamir <lgt986452565@gmail.com>
2023-05-16 19:56:21 +0800

    feat: disable session_lens load_on_setup, create command for search_session.

--- lua/modules/configs/editor/auto-session.lua
+++ lua/modules/configs/editor/auto-session.lua
@@ -7,5 +7,16 @@ return function()
 		auto_save_enabled = true,
 		auto_restore_enabled = true,
 		auto_session_suppress_dirs = nil,
+		session_lens = {
+			-- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+			load_on_setup = false,
+			theme_conf = { border = true },
+			previewer = false,
+		},
 	})
+
+	vim.api.nvim_create_user_command("SessionLenShow", function()
+		require("auto-session").setup_session_lens()
+		require("auto-session.session-lens").search_session()
+	end, { nargs = 0 })
 end
