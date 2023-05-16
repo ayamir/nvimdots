1b53c90eca5bb1039ef67667214c36483ff2d021
groveer <groveer@qq.com>
2023-05-16 21:45:51 +0800

    feat!: use executables on `$PATH` (#738)
    * fix: use commands in environment variables instead of absolute paths
    
    * fixup! fix: use commands in environment variables instead of absolute paths
    
    ---------
    
    Co-authored-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -110,12 +110,9 @@ local function load_options()
 	if not isempty(conda_prefix) then
 		vim.g.python_host_prog = conda_prefix .. "/bin/python"
 		vim.g.python3_host_prog = conda_prefix .. "/bin/python"
-	elseif global.is_mac then
-		vim.g.python_host_prog = "/usr/bin/python"
-		vim.g.python3_host_prog = "/usr/local/bin/python3"
 	else
-		vim.g.python_host_prog = "/usr/bin/python"
-		vim.g.python3_host_prog = "/usr/bin/python3"
+		vim.g.python_host_prog = "python"
+		vim.g.python3_host_prog = "python3"
 	end
 
 	for name, value in pairs(global_local) do
