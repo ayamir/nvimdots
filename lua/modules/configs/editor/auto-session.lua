351549c53476af854ab94aaa93ae9dfac045316d
ayamir <lgt986452565@gmail.com>
2023-05-16 22:49:41 +0800

    feat: load `SessionLens` manually via user command (#745)
    * feat: disable session_lens load_on_setup, create command for search_session.
    
    * perf: setup_session_lens only it not loaded.
    
    * perf: update command name and side note.
    
    ---------
    
    Co-authored-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/editor/auto-session.lua
+++ lua/modules/configs/editor/auto-session.lua
@@ -7,5 +7,18 @@ return function()
 		auto_save_enabled = true,
 		auto_restore_enabled = true,
 		auto_session_suppress_dirs = nil,
+		session_lens = {
+			-- If `load_on_setup` is set to false, please use `SessionLensToggle` to manually load this add-on.
+			load_on_setup = false,
+			theme_conf = { border = true },
+			previewer = false,
+		},
 	})
+
+	vim.api.nvim_create_user_command("SessionLensToggle", function()
+		if not package.loaded["auto-session.session-lens"] then
+			require("auto-session").setup_session_lens()
+		end
+		require("auto-session.session-lens").search_session()
+	end, { nargs = 0 })
 end
