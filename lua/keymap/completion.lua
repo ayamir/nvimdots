452b2c763ecfead8880f7832f306ec020f5aa7e8
ayamir <miracle_l@bupt.edu.cn>
2023-05-18 23:47:04 +0800

    remove reduntant note.

--- lua/keymap/completion.lua
+++ lua/keymap/completion.lua
@@ -5,7 +5,6 @@ local map_cmd = bind.map_cmd
 local map_callback = bind.map_callback
 
 local plug_map = {
-	-- LSP-related keymaps, work only when event = { "InsertEnter", "LspStart" }
 	["n|<A-f>"] = map_cmd("<Cmd>FormatToggle<CR>"):with_noremap():with_desc("Formater: Toggle format on save"),
 }
 bind.nvim_load_mapping(plug_map)
