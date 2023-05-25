750eef1f46ed83d4833f68f394e45909015206b1
ayamir <lgt986452565@gmail.com>
2023-05-25 10:48:08 +0800

    revert: use win32yank because clip can't handle UTF-8 string correctly.

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -101,14 +101,14 @@ local clipboard_config = function()
 		}
 	elseif global.is_wsl then
 		vim.g.clipboard = {
-			name = "psyank-wsl",
+			name = "win32yank-wsl",
 			copy = {
-				["+"] = "clip.exe",
-				["*"] = "clip.exe",
+				["+"] = "win32yank.exe -i --crlf",
+				["*"] = "win32yank.exe -i --crlf",
 			},
 			paste = {
-				["+"] = [[powershell.exe -NoProfile -NoLogo -NonInteractive -Command [console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
-				["*"] = [[powershell.exe -NoProfile -NoLogo -NonInteractive -Command [console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
+				["+"] = "win32yank.exe -o --lf",
+				["*"] = "win32yank.exe -o --lf",
 			},
 			cache_enabled = 0,
 		}
