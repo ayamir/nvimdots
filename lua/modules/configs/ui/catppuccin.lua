915b41177a89b2aac3bfe3c2ecc93fe4939f7bd2
groveer <groveer@qq.com>
2023-06-19 13:24:10 +0800

    fix(ui): telescope ui not adapt transparent background

--- lua/modules/configs/ui/catppuccin.lua
+++ lua/modules/configs/ui/catppuccin.lua
@@ -140,15 +140,41 @@ return function()
 					TroubleNormal = { bg = transparent_background and cp.none or cp.base },
 
 					-- For telescope.nvim
-					TelescopeBorder = { fg = cp.mantle, bg = cp.mantle },
-					TelescopePromptBorder = { fg = cp.surface0, bg = cp.surface0 },
-					TelescopePromptNormal = { fg = cp.text, bg = cp.surface0 },
-					TelescopePromptPrefix = { fg = cp.flamingo, bg = cp.surface0 },
-					TelescopeNormal = { bg = cp.mantle },
-					TelescopePreviewTitle = { fg = cp.base, bg = cp.green },
-					TelescopePromptTitle = { fg = cp.base, bg = cp.red },
-					TelescopeResultsTitle = { fg = cp.mantle, bg = cp.mantle },
-					TelescopeSelection = { fg = cp.text, bg = cp.surface0 },
+					TelescopeBorder = {
+						fg = transparent_background and cp.blue or cp.mantle,
+						bg = transparent_background and cp.none or cp.mantle,
+					},
+					TelescopePromptBorder = {
+						fg = transparent_background and cp.blue or cp.surface0,
+						bg = transparent_background and cp.none or cp.surface0,
+					},
+					TelescopePromptNormal = {
+						fg = cp.text,
+						bg = transparent_background and cp.none or cp.surface0,
+					},
+					TelescopePromptPrefix = {
+						fg = cp.flamingo,
+						bg = transparent_background and cp.none or cp.surface0,
+					},
+					TelescopeNormal = {
+						bg = transparent_background and cp.none or cp.mantle,
+					},
+					TelescopePreviewTitle = {
+						fg = transparent_background and cp.green or cp.base,
+						bg = transparent_background and cp.none or cp.green,
+					},
+					TelescopePromptTitle = {
+						fg = transparent_background and cp.red or cp.base,
+						bg = transparent_background and cp.none or cp.red,
+					},
+					TelescopeResultsTitle = {
+						fg = cp.mantle,
+						bg = transparent_background and cp.none or cp.mantle,
+					},
+					TelescopeSelection = {
+						fg = cp.green,
+						bg = transparent_background and cp.none or cp.surface0,
+					},
 					TelescopeResultsDiffAdd = { fg = cp.green },
 					TelescopeResultsDiffChange = { fg = cp.yellow },
 					TelescopeResultsDiffDelete = { fg = cp.red },
