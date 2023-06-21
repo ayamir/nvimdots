5b478f5c49943f14bac8d811c260d1944b7b475c
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-06-22 00:09:38 +0800

    feat(formatting): format disabled dirs may contain regexps

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -26,9 +26,11 @@ settings["diagnostics_virtual_text"] = true
 settings["diagnostics_level"] = "Hint"
 
 -- Set the format disabled directories here, files under these dirs won't be formatted on save.
+--- NOTE: String(s) may contain regular expressions (grammar: vim). |regexp|
+--- NOTE: Directories are automatically normalized. |vim.fs.normalize()|
 ---@type string[]
 settings["format_disabled_dirs"] = {
-	home .. "/format_disabled_dir_under_home",
+	"~/format_disabled_dir",
 }
 
 -- Set it to false if you don't use nvim to open big files.
