0af8f4b322adcf0a4a176e6b20003fad4e11f494
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-07-02 16:39:04 +0800

    fix(options): correct scroll behaviors of (h)splits (#835)
    Just found out that we removed `stabilize.nvim` but forgot to set the
    corresponding option :)

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -73,7 +73,7 @@ local function load_options()
 		smarttab = true,
 		softtabstop = 4,
 		splitbelow = true,
-		splitkeep = "cursor",
+		splitkeep = "screen",
 		splitright = true,
 		startofline = false,
 		swapfile = false,
