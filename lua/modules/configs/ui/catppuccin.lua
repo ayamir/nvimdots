10005d05d2b54aa0e51a7a728ea77812e706eb0a
Null Chilly <56817415+nullchilly@users.noreply.github.com>
2023-07-10 23:20:57 +0700

    feat(catppuccin): use upstream nvchad style (#855)

--- lua/modules/configs/ui/catppuccin.lua
+++ lua/modules/configs/ui/catppuccin.lua
@@ -83,7 +83,7 @@ return function()
 			semantic_tokens = true,
 			symbols_outline = false,
 			telekasten = false,
-			telescope = true,
+			telescope = { enabled = true, style = "nvchad" },
 			treesitter_context = true,
 			ts_rainbow = false,
 			vim_sneak = false,
@@ -148,41 +148,6 @@ return function()
 					TroubleNormal = { bg = transparent_background and cp.none or cp.base },
 
 					-- For telescope.nvim
-					TelescopeBorder = {
-						fg = transparent_background and cp.blue or cp.mantle,
-						bg = transparent_background and cp.none or cp.mantle,
-					},
-					TelescopePromptBorder = {
-						fg = transparent_background and cp.blue or cp.surface0,
-						bg = transparent_background and cp.none or cp.surface0,
-					},
-					TelescopePromptNormal = {
-						fg = cp.text,
-						bg = transparent_background and cp.none or cp.surface0,
-					},
-					TelescopePromptPrefix = {
-						fg = cp.flamingo,
-						bg = transparent_background and cp.none or cp.surface0,
-					},
-					TelescopeNormal = {
-						bg = transparent_background and cp.none or cp.mantle,
-					},
-					TelescopePreviewTitle = {
-						fg = transparent_background and cp.green or cp.base,
-						bg = transparent_background and cp.none or cp.green,
-					},
-					TelescopePromptTitle = {
-						fg = transparent_background and cp.red or cp.base,
-						bg = transparent_background and cp.none or cp.red,
-					},
-					TelescopeResultsTitle = {
-						fg = cp.mantle,
-						bg = transparent_background and cp.none or cp.mantle,
-					},
-					TelescopeSelection = {
-						fg = transparent_background and cp.subtext0 or cp.text,
-						bg = transparent_background and cp.none or cp.surface0,
-					},
 					TelescopeResultsDiffAdd = { fg = cp.green },
 					TelescopeResultsDiffChange = { fg = cp.yellow },
 					TelescopeResultsDiffDelete = { fg = cp.red },
