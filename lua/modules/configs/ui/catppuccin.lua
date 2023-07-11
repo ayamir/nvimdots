54b2d888fcc69f1ba13850f7c35167ba5639afdc
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-07-11 17:43:45 +0800

    feat(lspsaga): support v0.3

--- lua/modules/configs/ui/catppuccin.lua
+++ lua/modules/configs/ui/catppuccin.lua
@@ -129,13 +129,6 @@ return function()
 						bg = transparent_background and cp.none or cp.mantle,
 					},
 
-					-- For lspsaga.nvim
-					OutlineNormal = { bg = transparent_background and cp.none or cp.mantle },
-					OutlineWinSeparator = {
-						bg = transparent_background and cp.none or cp.base,
-						fg = transparent_background and cp.surface1 or cp.base,
-					},
-
 					-- For fidget
 					FidgetTask = { bg = cp.none, fg = cp.surface2 },
 					FidgetTitle = { fg = cp.blue, style = { "bold" } },
