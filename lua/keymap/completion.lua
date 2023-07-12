5abcd0e0270bfc3bab9ffee1b7e3883503f58a8c
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-07-12 15:08:21 +0800

    feat(lspsaga): support v0.3 (#857)

--- lua/keymap/completion.lua
+++ lua/keymap/completion.lua
@@ -18,7 +18,9 @@ function mapping.lsp(buf)
 		["n|go"] = map_cr("Lspsaga outline"):with_buffer(buf):with_desc("lsp: Toggle outline"),
 		["n|g["] = map_cr("Lspsaga diagnostic_jump_prev"):with_buffer(buf):with_desc("lsp: Prev diagnostic"),
 		["n|g]"] = map_cr("Lspsaga diagnostic_jump_next"):with_buffer(buf):with_desc("lsp: Next diagnostic"),
-		["n|<leader>ld"] = map_cr("Lspsaga show_line_diagnostics"):with_buffer(buf):with_desc("lsp: Line diagnostic"),
+		["n|<leader>ld"] = map_cr("Lspsaga show_line_diagnostics ++unfocus")
+			:with_buffer(buf)
+			:with_desc("lsp: Line diagnostic"),
 		["n|gs"] = map_callback(function()
 			vim.lsp.buf.signature_help()
 		end):with_desc("lsp: Signature help"),
@@ -28,7 +30,7 @@ function mapping.lsp(buf)
 		["nv|ga"] = map_cr("Lspsaga code_action"):with_buffer(buf):with_desc("lsp: Code action for cursor"),
 		["n|gd"] = map_cr("Lspsaga peek_definition"):with_buffer(buf):with_desc("lsp: Preview definition"),
 		["n|gD"] = map_cr("Lspsaga goto_definition"):with_buffer(buf):with_desc("lsp: Goto definition"),
-		["n|gh"] = map_cr("Lspsaga lsp_finder"):with_buffer(buf):with_desc("lsp: Show reference"),
+		["n|gh"] = map_cr("Lspsaga finder"):with_buffer(buf):with_desc("lsp: Show reference"),
 		["n|<leader>ci"] = map_cr("Lspsaga incoming_calls"):with_buffer(buf):with_desc("lsp: Show incoming calls"),
 		["n|<leader>co"] = map_cr("Lspsaga outgoing_calls"):with_buffer(buf):with_desc("lsp: Show outgoing calls"),
 	}
--- lua/modules/utils/icons.lua
+++ lua/modules/utils/icons.lua
@@ -14,6 +14,7 @@ local data = {
 		Folder = "󰉋",
 		Function = "󰊕",
 		Interface = "",
+		Implementation = "",
 		Keyword = "󰌋",
 		Method = "󰆧",
 		Module = "",
