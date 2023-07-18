4785ff4f8723e331e6f572037b9536f9711e91ab
Charles Chiu <charleschiu@hey.com>
2023-07-18 09:50:02 +0800

    perf(pack): update `nvim-bufdel` lazy-loading events.
    Signed-off-by: Charles Chiu <charleschiu@hey.com>

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -36,7 +36,7 @@ editor["LunarVim/bigfile.nvim"] = {
 }
 editor["ojroques/nvim-bufdel"] = {
 	lazy = true,
-	event = "BufReadPre",
+	cmd = { "BufDel", "BufDelAll", "BufDelOthers" },
 }
 editor["rhysd/clever-f.vim"] = {
 	lazy = true,
