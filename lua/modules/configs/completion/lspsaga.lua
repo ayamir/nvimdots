8c065c1dbf98e08a8ac1cf80ab698b310f59ba97
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-07-18 17:05:43 +0800

    feat(lspsaga): enable titles for Nvim >=0.9 (#875)

--- lua/modules/configs/completion/lspsaga.lua
+++ lua/modules/configs/completion/lspsaga.lua
@@ -149,7 +149,7 @@ return function()
 			frequency = 12,
 		},
 		ui = {
-			title = false,
+			title = true,
 			devicon = true,
 			border = "single", -- Can be single, double, rounded, solid, shadow.
 			actionfix = icons.ui.Spell,
