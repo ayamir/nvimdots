5cbc229190ff0ada6fbbfd6fc47b4dd7ec7fa5f4
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 <yu.carl@outlook.com>
2023-07-23 20:21:44 +0100

    stop generate wrong colorscheme for vistakind

--- lua/modules/configs/tool/vista.lua
+++ lua/modules/configs/tool/vista.lua
@@ -1,5 +1,5 @@
 return function()
-    require("modules.utils").gen_vistakind_hl()
+    -- require("modules.utils").gen_vistakind_hl()
 	require("vista-nvim").setup({
 		theme = "type",
 		-- TODO: fix width, wirter.lua clean_path
