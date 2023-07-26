bd19bb8f07cc4c18d429e58b7e24d78793cd17be
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 <yu.carl@outlook.com>
2023-07-26 20:56:24 +0100

    create latex auto end function and bind with ]]

--- lua/keymap/lang.lua
+++ lua/keymap/lang.lua
@@ -11,10 +11,43 @@ local safe_call_nabla = function()
 	end
 end
 
+local latex_auto_end = function()
+	vim.cmd("stopinsert")
+
+	-- Get the current line and cursor position
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line = vim.api.nvim_get_current_line()
+
+	-- Find the start and end of the \begin{...} tag
+	local s, e = line:find("\\begin{%a+}")
+	if s == nil or e == nil then
+		return
+	end
+
+	-- Extract the environment name
+	local env = line:sub(s + 7, e - 1)
+
+	-- Insert \end{...} after the current line
+	vim.api.nvim_buf_set_lines(0, row, row, false, { "" })
+	vim.api.nvim_buf_set_lines(0, row + 1, row + 1, false, { "\\end{" .. env .. "}" })
+
+	-- Move the cursor back to its original position
+	vim.api.nvim_win_set_cursor(0, { row + 1, col })
+
+	-- Schedule the re-entry to insert mode and feeding the tab key
+	vim.schedule(function()
+		vim.cmd("startinsert")
+		vim.cmd("call feedkeys('\t')")
+	end)
+end
+
 local plug_map = {
 	-- Plugin MarkdownPreview
 	["n|<F12>"] = map_cr("MarkdownPreviewToggle"):with_noremap():with_silent():with_desc("tool: Preview markdown"),
 	["n|<leader>0"] = map_callback(safe_call_nabla):with_noremap():with_silent():with_desc("tool: Preview markdown"),
+
+	-- Auto create endwise for latex file
+	["i|]]"] = map_callback(latex_auto_end):with_noremap():with_silent():with_desc("tool: Auto create end for latex"),
 }
 
 bind.nvim_load_mapping(plug_map)
