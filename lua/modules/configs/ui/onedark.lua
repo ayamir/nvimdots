63c3cccbbb47b02768b863f23fe2528e8e6dff2e
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 <yu.carl@outlook.com>
2023-07-26 13:51:44 +0100

    minor change

--- lua/modules/configs/ui/onedark.lua
+++ lua/modules/configs/ui/onedark.lua
@@ -3,13 +3,13 @@ return function()
 		style = "darker",
 		highlights = {
 			-- For native lsp configs
-			MatchParen = { bg = "#727169" },
+			-- MatchParen = { bg = "#727169" },
 			Search = { bg = "#99D1DB", fg = "#282c34", ctermfg = 0, ctermbg = 11 },
 			IncSearch = { bg = "#99D1DB", fg = "#282c34", ctermfg = 0, ctermbg = 11 },
+			CurSearch = { bg = "#99D1DB", fg = "#282c34", ctermfg = 0, ctermbg = 11 },
 			NeoTreeDirectoryIcon = { fg = "#8094b4" },
 			NeoTreeIndentMarker = { fg = "#abb2bf" },
 			NeoTreeExpander = { fg = "#abb2bf" },
-			CurSearch = { bg = "#99D1DB", fg = "#282c34", ctermfg = 0, ctermbg = 11 },
 			FinderPreviewSearch = { bg = "#e95678", fg = "#1f2329" },
 			EndOfBuffer = { bg = "none", fg = "#1f2329" },
 			LspInfoBorder = { fg = "#99a3b3" },
