98bbe98d2d7aaac90177b57d0a1564353b0672e3
Sumi-Sumi <dragon511southern@gmail.com>
2023-07-28 15:04:48 +0900

    feat(flake.nix): Change module name.

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   # This provides only NixOS module
   # As of 2023/07/24, you need to depend on nixpkgs-unstable.
   # because "doq" is not included in the stable version.
-  description = "Provide home-manager module for ayamir/neovim";
+  description = "Provide nixosModules for ayamir/nvimdots";
 
   inputs = { };
 
   outputs = inputs: {
     nixosModules = {
-      for-hm = ./nixos;
+      nvimdots = ./nixos;
     };
   };
 }
