853428db8f63ad5d3eec4c0c65474841c5af3659
Charles Chiu <charleschiu@hey.com>
2023-07-28 10:15:18 +0800

    perf(lua_ls): let `null_ls` handle formatting
    Signed-off-by: Charles Chiu <charleschiu@hey.com>

--- lua/modules/configs/completion/servers/lua_ls.lua
+++ lua/modules/configs/completion/servers/lua_ls.lua
@@ -3,6 +3,7 @@ return {
 	cmd = { "lua-language-server" },
 	settings = {
 		Lua = {
+			format = { enable = false },
 			telemetry = { enable = false },
 			-- Do not override treesitter lua highlighting with lua_ls' highlighting
 			semantic = { enable = false },
