6b7d5a6e408b9d6e9a3345c3497ced6f8293f134
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 <yu.carl@outlook.com>
2023-07-29 23:04:19 +0100

    update lsp keymap

--- lua/keymap/completion.lua
+++ lua/keymap/completion.lua
@@ -25,22 +25,32 @@ function mapping.lsp(buf)
 		["n|<leader>li"] = map_cr("LspInfo"):with_buffer(buf):with_desc("lsp: Info"),
 		["n|<leader>lr"] = map_cr("LspRestart"):with_buffer(buf):with_nowait():with_desc("lsp: Restart"),
 		["n|<leader>gf"] = map_cr("Lspsaga lsp_finder"):with_buffer(buf):with_desc("lsp: Toggle finder"),
-		["n|<leader>ld"] = map_cr("Lspsaga show_line_diagnostics ++unfocus")
-			:with_buffer(buf)
-			:with_desc("lsp: Line diagnostic"),
-		["i|<c-s>"] = map_callback(function()
-			vim.lsp.buf.signature_help()
-		end):with_desc("lsp: Signature help"),
-		["n|<leader>rn"] = map_cr("Lspsaga rename"):with_buffer(buf):with_desc("lsp: Rename in file range"),
-		["n|<leader>rN"] = map_cr("Lspsaga rename ++project")
-			:with_buffer(buf)
-			:with_desc("lsp: Rename in project range"),
-		["n|K"] = map_cr("lua vim.lsp.buf.hover()"):with_buffer(buf):with_desc("lsp: Show doc"),
-		["nv|<leader>ca"] = map_cr("Lspsaga code_action"):with_buffer(buf):with_desc("lsp: Code action for cursor"),
 		["n|<leader>gp"] = map_cr("Lspsaga peek_definition"):with_buffer(buf):with_desc("lsp: Preview definition"),
 		["n|<leader>gi"] = map_cr("Lspsaga incoming_calls"):with_buffer(buf):with_desc("lsp: Show incoming calls"),
 		["n|<leader>go"] = map_cr("Lspsaga outgoing_calls"):with_buffer(buf):with_desc("lsp: Show outgoing calls"),
+		["n|<leader>ca"] = map_cr("Lspsaga code_action"):with_buffer(buf):with_desc("lsp: Code action for cursor"),
+		["n|<leader>ld"] = map_cr("Lspsaga show_line_diagnostics ++unfocus")
+			:with_buffer(buf)
+			:with_desc("lsp: Line diagnostic"),
+		["n|<TAB>"] = map_cr(":lua vim.lsp.buf.hover()<CR>"):with_buffer(buf):with_desc("lsp: Toggle hover"),
+		["n|K"] = map_cr("lua vim.lsp.buf.hover()"):with_buffer(buf):with_desc("lsp: Show doc"),
+		["n|<leader>rn"] = map_cmd("<cmd>lua vim.lsp.buf.rename()<CR>"):with_buffer(buf):with_desc("lsp: Rename"),
+		["n|gr"] = map_cmd("<cmd>lua vim.lsp.buf.rename()<CR>"):with_buffer(buf):with_desc("lsp: Rename"),
 		["n|<leader>hd"] = map_callback(hide_diagnostic):with_buffer(buf):with_desc("lsp: Toggle hide diagnostic"),
+		["i|<c-s>"] = map_callback(function()
+			vim.lsp.buf.signature_help()
+		end):with_desc("lsp: Signature help"),
+		["n|gd"] = map_cmd("<cmd>lua vim.lsp.buf.definition()<CR>"):with_buffer(buf):with_desc("lsp: Goto definition"),
+		["n|gq"] = map_cmd("<cmd>lua vim.diagnostic.setqflist()<CR>")
+			:with_buffer(buf)
+			:with_desc("lsp: Toggle diagnostic quickfix"),
+		["n|gh"] = map_cmd("<cmd>lua vim.lsp.buf.references()<CR>"):with_buffer(buf):with_desc("lsp: Goto references"),
+		["n|]d"] = map_cr('<cmd>lua vim.diagnostic.goto_next({ popup_opts = { border = "single" }})<CR>')
+			:with_buffer(buf)
+			:with_desc("lsp: Goto next diagnostic"),
+		["n|[d"] = map_cr('<cmd>lua vim.diagnostic.goto_prev({ popup_opts = { border = "single" }})<CR>')
+			:with_buffer(buf)
+			:with_desc("lsp: Goto prev diagnostic"),
 	}
 	bind.nvim_load_mapping(map)
 end
