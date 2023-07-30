915a7ba6dfe724fe192e98d814f866c8cb7f542d
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 <yu.carl@outlook.com>
2023-07-30 18:53:22 +0100

    config toggleterm

--- lua/modules/configs/tool/toggleterm.lua
+++ lua/modules/configs/tool/toggleterm.lua
@@ -37,6 +37,7 @@ return function()
 		start_in_insert = true,
 		insert_mappings = true, -- whether or not the open mapping applies in insert mode
 		persist_size = true,
+		persist_mode = false,
 		direction = "horizontal",
 		close_on_exit = true, -- close the terminal window when the process exits
 		shell = vim.o.shell, -- change the default shell
