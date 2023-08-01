12d63f54a940ac496a290deb9c921198e0c6e362
Sumi-Sumi <dragon511southern@gmail.com>
2023-08-01 17:37:50 +0900

    hotfix(pack): Incorrect value is added when there is no user-defined plugin

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -33,7 +33,9 @@ function Lazy:load_plugins()
 		local list = {}
 		local plugins_list = vim.split(fn.glob(modules_dir .. "/plugins/*.lua"), "\n")
 		local user_plugins_list = vim.split(fn.glob(user_modules_dir .. "/plugins/*.lua"), "\n")
-		plugins_list = vim.list_extend(plugins_list, user_plugins_list)
+		if user_plugins_list[1] ~= "" then
+			plugins_list = vim.list_extend(plugins_list, user_plugins_list)
+		end
 		if type(plugins_list) == "table" then
 			for _, f in ipairs(plugins_list) do
 				-- fill list with `plugins/*.lua`'s path used for later `require` like this:
