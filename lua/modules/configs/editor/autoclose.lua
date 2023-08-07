ayamir/nvimdots

commit 7a01d7a45c26a7b89761c7dc11a5d8413afe7d1a

fix: only disable autoclose for specific filetypes (#916)
* fix: set autoclose properly for cpp&rust.

Signed-off-by: ayamir <lgt986452565@gmail.com>

* fixup!: clean code.

---------

Signed-off-by: ayamir <lgt986452565@gmail.com>

lua/modules/configs/editor/autoclose.lua
@@ -5,14 +5,15 @@ return function()
 			["["] = { escape = false, close = true, pair = "[]" },
 			["{"] = { escape = false, close = true, pair = "{}" },
 
-			[">"] = { escape = true, close = false, pair = "<>" },
 			[")"] = { escape = true, close = false, pair = "()" },
 			["]"] = { escape = true, close = false, pair = "[]" },
 			["}"] = { escape = true, close = false, pair = "{}" },
 
 			['"'] = { escape = true, close = true, pair = '""' },
-			["'"] = { escape = true, close = true, pair = "''" },
 			["`"] = { escape = true, close = true, pair = "``" },
+			["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = { "rust" } },
+			["<"] = { escape = true, close = true, pair = "<>", disabled_filetypes = { "cpp" } },
+			[">"] = { escape = true, close = true, pair = "<>", disabled_filetypes = { "cpp" } },
 		},
 		options = {
 			disabled_filetypes = { "big_file_disabled_ft" },

commit bc73f31600a9222f9769b2332970ff108d79ea76

revert(autoclose): temporal fix for regression introduced by #916

lua/modules/configs/editor/autoclose.lua
@@ -13,7 +13,6 @@ return function()
 			['"'] = { escape = true, close = true, pair = '""' },
 			["`"] = { escape = true, close = true, pair = "``" },
 			["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = { "rust" } },
-			["<"] = { escape = true, close = true, pair = "<>", disabled_filetypes = { "cpp" } },
 		},
 		options = {
 			disabled_filetypes = { "big_file_disabled_ft" },