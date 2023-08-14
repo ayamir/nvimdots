ed88c0a4ffc5f2e66e65f94113eada80542f0f11
Charles Chiu <charleschiu@hey.com>
2023-08-14 14:13:38 +0800

    chore: update comments
    Signed-off-by: Charles Chiu <charleschiu@hey.com>

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -39,8 +39,8 @@ function Lazy:load_plugins()
 		local user_plugins_list = vim.split(fn.glob(user_config_dir .. "/plugins/*.lua"), "\n", { trimempty = true })
 		vim.list_extend(plugins_list, user_plugins_list)
 		for _, f in ipairs(plugins_list) do
-			-- fill list with `plugins/*.lua`'s path used for later `require` like this:
-			-- list[#list + 1] = "plugins/completion.lua"
+			-- aggregate the plugins from `/plugins/*.lua` and `/user/plugins/*.lua` to a plugin list of a certain field for later `require` action.
+			-- current fields contains: completion, editor, lang, tool, ui
 			list[#list + 1] = f:find(modules_dir) and f:sub(#modules_dir - 6, -1) or f:sub(#user_config_dir - 3, -1)
 		end
 		return list
@@ -49,8 +49,7 @@ function Lazy:load_plugins()
 	append_nativertp()
 
 	for _, m in ipairs(get_plugins_list()) do
-		-- require modules which returned in previous operation like this:
-		-- local modules = require("modules/plugins/completion.lua")
+		-- require modules returned from `get_plugins_list()` function.
 		local modules = require(m:sub(0, #m - 4))
 		if type(modules) == "table" then
 			for name, conf in pairs(modules) do
