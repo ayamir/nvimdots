87066d7e3e3cf1e5e61696229228333fb21e1004
Mohu <lgt986452565@gmail.com>
2023-10-23 12:00:03 +0800

    feat: customized `neovide` and `gui` config (#1033)
    * feat: move neovide config to settings.
    
    Signed-off-by: ayamir <lgt986452565@gmail.com>
    
    * docs: add comment for new entrys.
    
    Signed-off-by: ayamir <lgt986452565@gmail.com>
    
    * docs: add more NOTEs.
    
    Signed-off-by: ayamir <lgt986452565@gmail.com>
    
    ---------
    
    Signed-off-by: ayamir <lgt986452565@gmail.com>

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -161,4 +161,26 @@ settings["treesitter_deps"] = {
 	"yaml",
 }
 
+-- Settings for the neovim-guis like `neovide`, `neovim-qt`, etc.
+-- NOTE: Only supports the options listed below.
+settings["gui_config"] = {
+	font_name = "JetBrainsMono Nerd Font",
+	font_size = 12,
+}
+
+-- Settings for `neovide`.
+-- NOTE: Get the full configurable options: https://neovide.dev/configuration.html
+settings["neovide_config"] = {
+	refresh_rate = 120,
+	cursor_vfx_mode = "railgun",
+	no_idle = true,
+	cursor_animation_length = 0.03,
+	cursor_trail_length = 0.05,
+	cursor_antialiasing = true,
+	cursor_vfx_opacity = 200.0,
+	cursor_vfx_particle_lifetime = 1.2,
+	cursor_vfx_particle_speed = 20.0,
+	cursor_vfx_particle_density = 5.0,
+}
+
 return require("modules.utils").extend_config(settings, "user.settings")
