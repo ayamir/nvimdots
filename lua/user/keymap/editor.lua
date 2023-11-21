ab076adcf42f39aa59bd184a5b7c4eea3673ede6
channinghsu <xbc1241888704@gmail.com>
2023-11-21 15:24:21 +0800

    add keymap

--- lua/user/keymap/editor.lua
+++ lua/user/keymap/editor.lua
@@ -6,4 +6,5 @@ return {
 	-- Plugin: nvim-bufdel
 	["n|<leader>x"] = map_cr("BufDel"):with_noremap():with_silent():with_desc("buffer: Close current"),
 	["n|<leader>v"] = map_cr("DiffviewOpen"):with_silent():with_noremap():with_desc("git: Show diff"),
+	["n|<leader>V"] = map_cr("DiffviewClose"):with_silent():with_noremap():with_desc("git: Close diff"),
 }
