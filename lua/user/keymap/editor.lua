57683d91950f2752382c2067c99e3a38aa25c45e
channinghsu <xbc1241888704@gmail.com>
2023-11-21 15:24:02 +0800

    add flash.vim key & update format

--- lua/user/keymap/editor.lua
+++ lua/user/keymap/editor.lua
@@ -1,9 +1,9 @@
 local bind = require("keymap.bind")
 local map_cr = bind.map_cr
+local map_cmd = bind.map_cmd
 
 return {
 	-- Plugin: nvim-bufdel
 	["n|<leader>x"] = map_cr("BufDel"):with_noremap():with_silent():with_desc("buffer: Close current"),
 	["n|<leader>v"] = map_cr("DiffviewOpen"):with_silent():with_noremap():with_desc("git: Show diff"),
-	["n|<leader>V"] = map_cr("DiffviewClose"):with_silent():with_noremap():with_desc("git: Close diff"),
 }
