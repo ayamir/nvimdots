26b9b4344fe2c3c5b2f978f7db82946e124e933e
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-12-04 19:06:09 +0800

    feat(plugins): migrate from `null-ls` to `none-ls`
    IMO we can switch to `none-ls` before we decide on the best alternative
    to `null-ls` b/c it's a drop-in replacement for `null-ls`, and we
    can continue to receive future bug fixes as well.

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -112,7 +112,7 @@ settings["lsp_deps"] = {
 -- Set the general-purpose servers that will be installed during bootstrap here.
 -- Check the below link for all supported sources.
 -- in `code_actions`, `completion`, `diagnostics`, `formatting`, `hover` folders:
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins
+-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins
 ---@type string[]
 settings["null_ls_deps"] = {
 	"clang_format",
