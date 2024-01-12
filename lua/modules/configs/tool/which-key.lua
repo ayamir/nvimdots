071100a60c34f8d2c34ad5afb6eb0f18fae04345
Chui <blown.away@qq.com>
2024-01-12 14:53:01 +0800

    format code.

--- lua/modules/configs/tool/which-key.lua
+++ lua/modules/configs/tool/which-key.lua
@@ -1,36 +1,36 @@
 return function()
-	local icons = {
-		ui = require("modules.utils.icons").get("ui"),
-		misc = require("modules.utils.icons").get("misc"),
-	}
+  local icons = {
+    ui = require("modules.utils.icons").get("ui"),
+    misc = require("modules.utils.icons").get("misc")
+  }
 
-	require("modules.utils").load_plugin("which-key", {
-		plugins = {
-			presets = {
-				operators = false,
-				motions = false,
-				text_objects = false,
-				windows = false,
-				nav = false,
-				z = true,
-				g = true,
-			},
-		},
+  require("modules.utils").load_plugin("which-key", {
+    plugins = {
+      presets = {
+        operators = false,
+        motions = false,
+        text_objects = false,
+        windows = false,
+        nav = false,
+        z = true,
+        g = true
+      }
+    },
 
-		icons = {
-			breadcrumb = icons.ui.Separator,
-			separator = icons.misc.Vbar,
-			group = "",
-		},
+    icons = {
+      breadcrumb = icons.ui.Separator,
+      separator = icons.misc.Vbar,
+      group = ""
+    },
 
-		window = {
-			border = "none",
-			position = "bottom",
-			margin = { 1, 0, 1, 0 },
-			padding = { 1, 1, 1, 1 },
-			winblend = 0,
-		},
-	})
+    window = {
+      border = "none",
+      position = "bottom",
+      margin = {1, 0, 1, 0},
+      padding = {1, 1, 1, 1},
+      winblend = 0
+    }
+  })
 
-	require("modules.utils.keymap").which_key_register()
+  require("modules.utils.keymap").which_key_register()
 end
--- lua/modules/utils/keymap.lua
+++ lua/modules/utils/keymap.lua
@@ -4,7 +4,7 @@ local M = {}
 ---@param keys string
 ---@return string
 local function termcodes(keys)
-	return vim.api.nvim_replace_termcodes(keys, true, true, true)
+  return vim.api.nvim_replace_termcodes(keys, true, true, true)
 end
 
 ---Returns if two key sequence are equal or not.
@@ -12,7 +12,7 @@ end
 ---@param b string
 ---@return boolean
 local function keymap_equals(a, b)
-	return termcodes(a) == termcodes(b)
+  return termcodes(a) == termcodes(b)
 end
 
 ---Get map
@@ -20,51 +20,51 @@ end
 ---@param lhs string
 ---@return table
 local function get_map(mode, lhs)
-	for _, map in ipairs(vim.api.nvim_buf_get_keymap(0, mode)) do
-		if keymap_equals(map.lhs, lhs) then
-			vim.api.nvim_buf_del_keymap(0, mode, lhs)
-			return {
-				lhs = map.lhs,
-				rhs = map.rhs or "",
-				expr = map.expr == 1,
-				callback = map.callback,
-				noremap = map.noremap == 1,
-				script = map.script == 1,
-				silent = map.silent == 1,
-				nowait = map.nowait == 1,
-				buffer = true,
-			}
-		end
-	end
+  for _, map in ipairs(vim.api.nvim_buf_get_keymap(0, mode)) do
+    if keymap_equals(map.lhs, lhs) then
+      vim.api.nvim_buf_del_keymap(0, mode, lhs)
+      return {
+        lhs = map.lhs,
+        rhs = map.rhs or "",
+        expr = map.expr == 1,
+        callback = map.callback,
+        noremap = map.noremap == 1,
+        script = map.script == 1,
+        silent = map.silent == 1,
+        nowait = map.nowait == 1,
+        buffer = true
+      }
+    end
+  end
 
-	for _, map in ipairs(vim.api.nvim_get_keymap(mode)) do
-		if keymap_equals(map.lhs, lhs) then
-			vim.api.nvim_del_keymap(mode, lhs)
-			return {
-				lhs = map.lhs,
-				rhs = map.rhs or "",
-				expr = map.expr == 1,
-				callback = map.callback,
-				noremap = map.noremap == 1,
-				script = map.script == 1,
-				silent = map.silent == 1,
-				nowait = map.nowait == 1,
-				buffer = false,
-			}
-		end
-	end
+  for _, map in ipairs(vim.api.nvim_get_keymap(mode)) do
+    if keymap_equals(map.lhs, lhs) then
+      vim.api.nvim_del_keymap(mode, lhs)
+      return {
+        lhs = map.lhs,
+        rhs = map.rhs or "",
+        expr = map.expr == 1,
+        callback = map.callback,
+        noremap = map.noremap == 1,
+        script = map.script == 1,
+        silent = map.silent == 1,
+        nowait = map.nowait == 1,
+        buffer = false
+      }
+    end
+  end
 
-	return {
-		lhs = lhs,
-		rhs = lhs,
-		expr = false,
-		callback = nil,
-		noremap = true,
-		script = false,
-		silent = true,
-		nowait = false,
-		buffer = false,
-	}
+  return {
+    lhs = lhs,
+    rhs = lhs,
+    expr = false,
+    callback = nil,
+    noremap = true,
+    script = false,
+    silent = true,
+    nowait = false,
+    buffer = false
+  }
 end
 
 ---Returns the function constructed from the passed keymap object on call of
@@ -72,24 +72,24 @@ end
 ---@param map table keymap object
 ---@return function
 local function get_fallback(map)
-	return function()
-		local keys, fmode
-		if map.expr then
-			if map.callback then
-				keys = map.callback()
-			else
-				keys = vim.api.nvim_eval(map.rhs)
-			end
-		elseif map.callback then
-			map.callback()
-			return
-		else
-			keys = map.rhs
-		end
-		keys = termcodes(keys)
-		fmode = map.noremap and "in" or "im"
-		vim.api.nvim_feedkeys(keys, fmode, false)
-	end
+  return function()
+    local keys, fmode
+    if map.expr then
+      if map.callback then
+        keys = map.callback()
+      else
+        keys = vim.api.nvim_eval(map.rhs)
+      end
+    elseif map.callback then
+      map.callback()
+      return
+    else
+      keys = map.rhs
+    end
+    keys = termcodes(keys)
+    fmode = map.noremap and "in" or "im"
+    vim.api.nvim_feedkeys(keys, fmode, false)
+  end
 end
 
 -- Amends a mapping (i.e., allows fallback when certain conditions are met)
@@ -99,18 +99,14 @@ end
 ---@param rhs function
 ---@param opts? table
 local function amend(cond, mode, lhs, rhs, opts)
-	local map = get_map(mode, lhs)
-	local fallback = get_fallback(map)
-	local options = vim.deepcopy(opts) or {}
-	options.desc = table.concat({
-		"[" .. cond,
-		(options.desc and ": " .. options.desc or ""),
-		"]",
-		(map.desc and " / " .. map.desc or ""),
-	})
-	vim.keymap.set(mode, lhs, function()
-		rhs(fallback)
-	end, options)
+  local map = get_map(mode, lhs)
+  local fallback = get_fallback(map)
+  local options = vim.deepcopy(opts) or {}
+  options.desc = table.concat({"[" .. cond, (options.desc and ": " .. options.desc or ""), "]",
+                               (map.desc and " / " .. map.desc or "")})
+  vim.keymap.set(mode, lhs, function()
+    rhs(fallback)
+  end, options)
 end
 
 -- Completely replace a mapping
@@ -120,14 +116,14 @@ end
 ---@param opts? table
 ---@param buf? boolean|number
 local function replace(mode, lhs, rhs, opts, buf)
-	get_map(mode, lhs)
+  get_map(mode, lhs)
 
-	local options = vim.deepcopy(opts) or {}
-	if buf and type(buf) == "number" then
-		vim.api.nvim_buf_set_keymap(buf, mode, lhs, rhs, options)
-	else
-		vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-	end
+  local options = vim.deepcopy(opts) or {}
+  if buf and type(buf) == "number" then
+    vim.api.nvim_buf_set_keymap(buf, mode, lhs, rhs, options)
+  else
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  end
 end
 
 ---Amend the existing keymap.
@@ -137,13 +133,13 @@ end
 ---@param rhs function
 ---@param opts? table
 local function modes_amend(cond, mode, lhs, rhs, opts)
-	if type(mode) == "table" then
-		for _, m in ipairs(mode) do
-			amend(cond, m, lhs, rhs, opts)
-		end
-	else
-		amend(cond, mode, lhs, rhs, opts)
-	end
+  if type(mode) == "table" then
+    for _, m in ipairs(mode) do
+      amend(cond, m, lhs, rhs, opts)
+    end
+  else
+    amend(cond, mode, lhs, rhs, opts)
+  end
 end
 
 ---Replace the existing keymap.
@@ -153,13 +149,13 @@ end
 ---@param opts? table
 ---@param buf? boolean|number
 local function modes_replace(mode, lhs, rhs, opts, buf)
-	if type(mode) == "table" then
-		for _, m in ipairs(mode) do
-			replace(m, lhs, rhs, opts, buf)
-		end
-	else
-		replace(mode, lhs, rhs, opts, buf)
-	end
+  if type(mode) == "table" then
+    for _, m in ipairs(mode) do
+      replace(m, lhs, rhs, opts, buf)
+    end
+  else
+    replace(mode, lhs, rhs, opts, buf)
+  end
 end
 
 ---Amend the existing keymap.
@@ -167,71 +163,74 @@ end
 ---@param global_flag string
 ---@param mapping table<string, map_rhs>
 function M.amend(cond, global_flag, mapping)
-	for key, value in pairs(mapping) do
-		local modes, keymap = key:match("([^|]*)|?(.*)")
-		if type(value) == "table" then
-			local rhs = value.cmd
-			local options = value.options
-			modes_amend(cond, vim.split(modes, ""), keymap, function(fallback)
-				if _G[global_flag] then
-					local fmode = options.noremap and "in" or "im"
-					vim.api.nvim_feedkeys(termcodes(rhs), fmode, false)
-				else
-					fallback()
-				end
-			end, options)
-		end
-	end
+  for key, value in pairs(mapping) do
+    local modes, keymap = key:match("([^|]*)|?(.*)")
+    if type(value) == "table" then
+      local rhs = value.cmd
+      local options = value.options
+      modes_amend(cond, vim.split(modes, ""), keymap, function(fallback)
+        if _G[global_flag] then
+          local fmode = options.noremap and "in" or "im"
+          vim.api.nvim_feedkeys(termcodes(rhs), fmode, false)
+        else
+          fallback()
+        end
+      end, options)
+    end
+  end
 end
 
 ---Replace the existing keymap.
 ---@param mapping table<string, map_rhs>
 function M.replace(mapping)
-	for key, value in pairs(mapping) do
-		local modes, keymap = key:match("([^|]*)|?(.*)")
-		if type(value) == "table" then
-			local rhs = value.cmd
-			local options = value.options
-			local buffer = value.buffer
-			modes_replace(vim.split(modes, ""), keymap, rhs, options, buffer)
-		elseif value == "" or value == false then
-			for _, m in ipairs(vim.split(modes, "")) do
-				get_map(m, keymap)
-			end
-		end
-	end
+  for key, value in pairs(mapping) do
+    local modes, keymap = key:match("([^|]*)|?(.*)")
+    if type(value) == "table" then
+      local rhs = value.cmd
+      local options = value.options
+      local buffer = value.buffer
+      modes_replace(vim.split(modes, ""), keymap, rhs, options, buffer)
+    elseif value == "" or value == false then
+      for _, m in ipairs(vim.split(modes, "")) do
+        get_map(m, keymap)
+      end
+    end
+  end
 end
 
 --- Register queued which-key mappings
 function M.which_key_register()
-	if M.which_key_queue then
-		local wk_avail, wk = pcall(require, "which-key")
-		if wk_avail then
-			for registration, opts in pairs(M.which_key_queue) do
-				wk.register(registration, opts)
-			end
-			M.which_key_queue = nil
-		end
-	end
+  if M.which_key_queue then
+    local wk_avail, wk = pcall(require, "which-key")
+    if wk_avail then
+      for registration, opts in pairs(M.which_key_queue) do
+        wk.register(registration, opts)
+      end
+      M.which_key_queue = nil
+    end
+  end
 end
 
 --- Insert a prefix keymap into register queue.
 function M.insert_queue(prefix_keymap, mode, buffer)
-	local registration = {}
-	local options = {mode = mode, buffer = buffer}
-	local P = require("keymap.prefix")
-	if not P[prefix_keymap] then
-		return
-	end
-	--- set name with icon
-	registration[prefix_keymap] = {}
-	registration[prefix_keymap]["name"] = P[prefix_keymap:sub(1, #"<leader>x")]
-	if not M.which_key_queue then
-		M.which_key_queue = {}
-	end
-	if not M.which_key_queue[registration] then
-		M.which_key_queue[registration] = options
-	end
+  local registration = {}
+  local options = {
+    mode = mode,
+    buffer = buffer
+  }
+  local P = require("keymap.prefix")
+  if not P[prefix_keymap] then
+    return
+  end
+  --- set name with icon
+  registration[prefix_keymap] = {}
+  registration[prefix_keymap]["name"] = P[prefix_keymap:sub(1, #"<leader>x")]
+  if not M.which_key_queue then
+    M.which_key_queue = {}
+  end
+  if not M.which_key_queue[registration] then
+    M.which_key_queue[registration] = options
+  end
 end
 
 return M
