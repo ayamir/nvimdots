00b860ab2e7333e40f075e09ed853dcd816ed33f
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-01-15 12:56:40 +0800

    fix(rust): follow upstream changes
    See https://github.com/mrcjkb/rustaceanvim/commit/819ae2d889cca667e7410341c48955043be100a0

--- lua/modules/configs/lang/rust.lua
+++ lua/modules/configs/lang/rust.lua
@@ -1,7 +1,11 @@
 return function()
 	vim.g.rustaceanvim = {
 		-- Disable automatic DAP configuration to avoid conflicts with previous user configs
-		dap = { adapter = false, configuration = false },
+		dap = {
+			adapter = false,
+			configuration = false,
+			autoload_configurations = false,
+		},
 	}
 
 	require("modules.utils").load_plugin("rustaceanvim", nil, true)
