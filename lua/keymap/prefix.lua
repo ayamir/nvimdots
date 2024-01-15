4513ec3fcea818e6dcd7adffdc72f3187cc54695
CharlesChiuGit <charleschiu@hey.com>
2024-01-15 18:15:56 +0800

    chore(which-key): add more nerdfont for which-key prefix

--- lua/keymap/prefix.lua
+++ lua/keymap/prefix.lua
@@ -2,13 +2,21 @@ local icons = {
 	ui_sep = require("modules.utils.icons").get("ui", true),
 	misc_sep = require("modules.utils.icons").get("misc", true),
 	git_sep = require("modules.utils.icons").get("git", true),
+	cmp_sep = require("modules.utils.icons").get("cmp", true),
 }
 
 --- Need to expand by hand
 local prefix_desc = {
 	["<leader>b"] = icons.ui_sep.Buffer .. "Buffer",
+	["<leader>c"] = icons.ui_sep.Character .. "Character",
+	["<leader>d"] = icons.ui_sep.Bug .. "Debug",
+	["<leader>f"] = icons.ui_sep.Telescope .. "Fuzzy Find",
+	["<leader>h"] = icons.git_sep.Git .. "Git Hunk",
 	["<leader>l"] = icons.misc_sep.LspAvailable .. "Lsp",
-	["<leader>h"] = icons.git_sep.Git .. "Git",
+	["<leader>n"] = icons.ui_sep.FolderOpen .. "Nvim Tree",
+	["<leader>p"] = icons.ui_sep.Package .. "Package",
+	["<leader>s"] = icons.cmp_sep.tmux .. "Session",
+	["<leader>t"] = icons.misc_sep.LspAvailable .. "Lsp",
 }
 
 return prefix_desc
