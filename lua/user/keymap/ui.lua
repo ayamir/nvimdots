975966c4a68b1d861f5d89158488e5634148c1aa
sigma2647_macmini <1169446204@qq.com>
2024-02-28 04:30:31 +0800

    upda

---
+++ lua/user/keymap/ui.lua
@@ -0,0 +1,18 @@
+local bind = require("keymap.bind")
+local map_cr = bind.map_cr
+local map_cmd = bind.map_cmd
+local map_callback = bind.map_callback
+
+return {
+	-- Remove default keymap
+	-- ["n|<leader>nf"] = "",
+	-- ["n|<leader>nr"] = false,
+	-- Plugin: telescope
+	["n|<leader><S-cr>"] = map_callback(function()
+			_command_panel()
+		end)
+		:with_noremap()
+		:with_silent()
+		:with_desc("tool: Toggle command panel"),
+    ["n|<leader>e"] = map_cr("NvimTreeToggle"):with_noremap():with_silent(),
+}
