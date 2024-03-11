5935dbbe772020c40432174a6383c5156d9d847d
ayamir <lgt986452565@gmail.com>
2024-03-11 15:20:45 +0800

    fixup: install script, close #1195.
    Signed-off-by: ayamir <lgt986452565@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -173,7 +173,7 @@ check_nvim_version() {
 clone_by_https_or_ssh() {
 	if check_nvim_version "${REQUIRED_NVIM_VERSION_NIGHTLY}"; then
 		execute "git" "clone" "-b" "0.10" "${CLONE_ATTR[@]}" "$1" "${DEST_DIR}"
-	elif check_nvim_version "${REQUIRED_NVIM_VERSION}" then
+	elif check_nvim_version "${REQUIRED_NVIM_VERSION}"; then
 		execute "git" "clone" "-b" "main" "${CLONE_ATTR[@]}" "$1" "${DEST_DIR}"
 	elif check_nvim_version "${REQUIRED_NVIM_VERSION_LEGACY}"; then
 		warn "You have outdated Nvim installed (< ${REQUIRED_NVIM_VERSION})."
