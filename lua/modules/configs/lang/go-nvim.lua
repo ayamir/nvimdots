2222a6ef52d46cba08054ab5dc59f86e2bbcaea2
ayamir <lgt986452565@gmail.com>
2024-04-12 23:00:20 +0800

    chore: add notes for config options.
    Signed-off-by: ayamir <lgt986452565@gmail.com>

--- lua/modules/configs/lang/go-nvim.lua
+++ lua/modules/configs/lang/go-nvim.lua
@@ -1,15 +1,24 @@
 return function()
 	require("modules.utils").load_plugin("go", {
+		-- disable lsp related keymap of plugin itself
 		lsp_keymaps = false,
+		-- disable it to avoid conflicts of unified dap configuration
+		dap_debug = false,
 		dap_debug_keymap = false,
 		icons = false,
 		gofmt = "gopls",
 		goimports = "gopls",
+		-- set gofumpt as default formatter of gopls
 		lsp_gofumpt = true,
-		lsp_inlay_hints = { enable = false },
-		run_in_floaterm = true,
-		trouble = true,
+		lsp_document_formatting = true,
 		lsp_codelens = false,
+		-- disable inlay hints by default
+		lsp_inlay_hints = { enable = false },
+		-- show failed test info in trouble window
+		trouble = true,
+		-- show passed test info in notify message
+		run_in_floaterm = false,
+		-- gopls config
 		lsp_cfg = require("modules.configs.completion.servers.gopls"),
 	})
 end
