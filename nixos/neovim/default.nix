121b180c67f522c694f24f4ac933cb7279b18d25
MiSumiSumi <dragon511southern@gmail.com>
2024-05-06 14:16:03 +0900

    Fix/nixos (#1250)
    * feat: add .nixd.json
    
    add formatter setting for .nix file
    
    * fix(nixos): fix bug
    
    fixed an issue where conditional branching based on version was not appropriate.
    fixed extraHaskellPackages document
    filtered packages of buildEnv by unsupport system
    move luarocks to extraPackages from extraLuaPackages
    add tree-sitter cli
    
    * feat(nixos): add testEnv
    
    add environment to evaluate nvimdots in a nix environment.
    
    * feat(nixos): some fix and add check script
    
    add script to check whether binary of installed by mason link to `/nix/store`
    rename test.nix to testEnv.nix
    put `lazy-lock.json` under `$XDG_CONFIG_HOME/$NVIM_APPNAME`
    
    * feat(workflow): add update_flake
    
    add workflow of auto update flake.lock
    
    * fix(flake): fix ldd command
    
    * fix(nixos): show current using home-manager version
    
    ---------
    
    Signed-off-by: MiSumiSumi <dragon511southern@gmail.com>

--- nixos/neovim/default.nix
+++ nixos/neovim/default.nix
@@ -36,20 +36,18 @@ in
         extraHaskellPackages = mkOption {
           type = with types;
             let fromType = listOf package;
-            in
-            coercedTo fromType
+            in coercedTo fromType
               (flip warn const ''
-                Assigning a plain list to extraRPackages is deprecated.
-                    Please assign a function taking a package set as argument, so
-                        extraHaskellPackages = [ pkgs.haskellPackages.xxx ];
-                    should be
-                        extraHaskellPackages = hsPkgs: with hsPkgs; [ xxx ];
+                Assigning a plain list to extraHaskellPackages is deprecated.
+                       Please assign a function taking a package set as argument, so
+                         extraHaskellPackages = [ pkgs.haskellPackages.xxx ];
+                       should become
+                         extraHaskellPackages = ps: [ ps.xxx ];
               '')
               (functionTo fromType);
           default = _: [ ];
           defaultText = literalExpression "ps: [ ]";
-          example =
-            literalExpression "hsPkgs: with hsPkgs; [ haskell-language-server ]";
+          example = literalExpression "hsPkgs: with hsPkgs; [ mtl ]";
           description = ''
             The extra Haskell packages required for your plugins to work.
             This option accepts a function that takes a Haskell package set as an argument,
@@ -69,30 +67,29 @@ in
   config =
     let
       # Inspired from https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/programs/nix-ld.nix
-      build-dependent-pkgs = with pkgs;
-        [
-          # manylinux
-          acl
-          attr
-          bzip2
-          curl
-          glibc
-          libgcc.lib
-          libsodium
-          libssh
-          libxml2
-          openssl
-          stdenv.cc.cc
-          systemd
-          util-linux
-          xz
-          zlib
-          zstd
-          # Packages not included in `nix-ld`'s NixOSModule
-          glib
-          libcxx
-        ]
-        ++ cfg.extraDependentPackages;
+      build-dependent-pkgs = with pkgs; builtins.filter (package: !package.meta.unsupported) [
+        # manylinux
+        acl
+        attr
+        bzip2
+        curl
+        glibc
+        libsodium
+        libssh
+        libxml2
+        openssl
+        stdenv.cc.cc
+        stdenv.cc.cc.lib
+        systemd
+        util-linux
+        xz
+        zlib
+        zstd
+        # Packages not included in `nix-ld`'s NixOSModule
+        glib
+        libcxx
+      ]
+      ++ cfg.extraDependentPackages;
 
       makePkgConfigPath = x: makeSearchPathOutput "dev" "lib/pkgconfig" x;
       makeIncludePath = x: makeSearchPathOutput "dev" "include" x;
@@ -114,93 +111,90 @@ in
         ''PKG_CONFIG_PATH=''${PKG_CONFIG_PATH:+''${PKG_CONFIG_PATH}:}${neovim-build-deps}/include/pkgconfig''
       ];
     in
-    mkIf cfg.enable
-      {
-        xdg.configFile = {
-          "nvim/init.lua".source = ../../init.lua;
-          "nvim/lua".source = ../../lua;
-          "nvim/snips".source = ../../snips;
-          "nvim/tutor".source = ../../tutor;
-        } // lib.optionalAttrs cfg.bindLazyLock {
-          "nvim/lazy-lock.json".source = ../../lazy-lock.json;
-        };
-        home = {
-          packages = with pkgs; [
-            ripgrep
-          ];
-          shellAliases = optionalAttrs (cfg.setBuildEnv && (lib.versionOlder config.home.stateVersion "24.05")) {
-            nvim = concatStringsSep " " buildEnv + " nvim";
-          };
-        };
-
-        programs.neovim = {
-          enable = true;
-
-          withNodeJs = true;
-          withPython3 = true;
-          withRuby = false;
-          extraWrapperArgs = optionals (cfg.setBuildEnv && (lib.versionAtLeast config.home.stateVersion "24.05")) [
-            "--suffix"
-            "CPATH"
-            ":"
-            "${neovim-build-deps}/include"
-            "--suffix"
-            "CPLUS_INCLUDE_PATH"
-            ":"
-            "${neovim-build-deps}/include/c++/v1"
-            "--suffix"
-            "LD_LIBRARY_PATH"
-            ":"
-            "${neovim-build-deps}/lib"
-            "--suffix"
-            "LIBRARY_PATH"
-            ":"
-            "${neovim-build-deps}/lib"
-            "--suffix"
-            "PKG_CONFIG_PATH"
-            ":"
-            "${neovim-build-deps}/include/pkgconfig"
-            "--suffix"
-            "NIX_LD_LIBRARY_PATH"
-            ":"
-            "${neovim-build-deps}/lib"
-          ];
-
-          extraPackages = with pkgs;
-            [
-              # Dependent packages used by default plugins
-              doq
-            ]
-            ++ optionals cfg.withBuildTools [
-              cargo
-              clang
-              cmake
-              gcc
-              gnumake
-              go
-              ninja
-              pkg-config
-              yarn
-              lua51Packages.luarocks
-            ]
-            ++ optionals cfg.withHaskell [
-              (pkgs.writeShellApplication {
-                name = "stack";
-                text = ''
-                  exec "${pkgs.stack}/bin/stack" "--extra-include-dirs=${config.home.profileDirectory}/lib/nvim-depends/include" "--extra-lib-dirs=${config.home.profileDirectory}/lib/nvim-depends/lib" "$@"
-                '';
-              })
-              (haskellPackages.ghcWithPackages (ps: cfg.extraHaskellPackages pkgs.haskellPackages))
-            ];
-
-          extraPython3Packages = ps: with ps; [
-            docformatter
-            isort
-            pynvim
-          ];
-          # extraLuaPackages = ls: with ls; [
-          # luarocks # doesn't work, put in extraPackages
-          # ];
+    mkIf cfg.enable {
+      xdg.configFile = {
+        "nvim/init.lua".source = ../../init.lua;
+        "nvim/lua".source = ../../lua;
+        "nvim/snips".source = ../../snips;
+        "nvim/tutor".source = ../../tutor;
+      } // lib.optionalAttrs cfg.bindLazyLock {
+        "nvim/lazy-lock.json".source = ../../lazy-lock.json;
+      };
+      home = {
+        packages = with pkgs; [
+          ripgrep
+        ];
+        shellAliases = optionalAttrs (cfg.setBuildEnv && (lib.versionOlder config.home.stateVersion "24.05")) {
+          nvim = concatStringsSep " " buildEnv + " nvim";
         };
       };
+      programs.neovim = {
+        enable = true;
+
+        withNodeJs = true;
+        withPython3 = true;
+
+        extraPackages = with pkgs;
+          [
+            # Dependent packages used by default plugins
+            doq
+            tree-sitter
+          ]
+          ++ optionals cfg.withBuildTools [
+            cargo
+            clang
+            cmake
+            gcc
+            gnumake
+            go
+            lua51Packages.luarocks
+            ninja
+            pkg-config
+            yarn
+          ]
+          ++ optionals cfg.withHaskell [
+            (pkgs.writeShellApplication {
+              name = "stack";
+              text = ''
+                exec "${pkgs.stack}/bin/stack" "--extra-include-dirs=${config.home.profileDirectory}/lib/nvim-depends/include" "--extra-lib-dirs=${config.home.profileDirectory}/lib/nvim-depends/lib" "$@"
+              '';
+            })
+            (haskellPackages.ghcWithPackages (ps: cfg.extraHaskellPackages ps))
+          ];
+
+        extraPython3Packages = ps: with ps; [
+          docformatter
+          isort
+          pynvim
+        ];
+      }
+      // lib.optionalAttrs (lib.versionAtLeast config.home.stateVersion "24.05") {
+        extraWrapperArgs = lib.optionals cfg.setBuildEnv [
+          "--suffix"
+          "CPATH"
+          ":"
+          "${neovim-build-deps}/include"
+          "--suffix"
+          "CPLUS_INCLUDE_PATH"
+          ":"
+          "${neovim-build-deps}/include/c++/v1"
+          "--suffix"
+          "LD_LIBRARY_PATH"
+          ":"
+          "${neovim-build-deps}/lib"
+          "--suffix"
+          "LIBRARY_PATH"
+          ":"
+          "${neovim-build-deps}/lib"
+          "--suffix"
+          "PKG_CONFIG_PATH"
+          ":"
+          "${neovim-build-deps}/include/pkgconfig"
+          "--suffix"
+          "NIX_LD_LIBRARY_PATH"
+          ":"
+          "${neovim-build-deps}/lib"
+        ];
+      };
+    };
 }
