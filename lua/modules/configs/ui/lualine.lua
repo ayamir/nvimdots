a55678e8594c42511d72f685ce208bf79c57de7c
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-06-10 00:23:59 +0800

    Revert "fix(lualine): ensure all components are properly aligned"
    This reverts commit 9be7ffedd1e5f59dd911e0de70d9cfdf9fcd8ae4.

--- lua/modules/configs/ui/lualine.lua
+++ lua/modules/configs/ui/lualine.lua
@@ -265,7 +265,6 @@ return function()
 					colored = true,
 					icon_only = false,
 					icon = { align = "left" },
-					padding = { left = 2, right = 1 },
 				},
 				components.file_status,
 				vim.tbl_extend("force", components.separator, {
