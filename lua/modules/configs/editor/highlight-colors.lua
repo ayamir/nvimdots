8024cc73cc4bda57fea39326adfc452774b4567e
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-06-11 15:27:17 +0800

    fix: invoke `utils.load_plugin`
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/editor/highlight-colors.lua
+++ lua/modules/configs/editor/highlight-colors.lua
@@ -1,16 +1,31 @@
 return function()
-	require("nvim-highlight-colors").setup({
-		---Render style
-		---@usage 'background'|'foreground'|'virtual'
+	require("modules.utils").load_plugin("nvim-highlight-colors", {
 		render = "background",
-
-		---Set virtual symbol (requires render to be set to 'virtual')
-		-- virtual_symbol = "■",
-
-		---Highlight named colors, e.g. 'green'
+		enable_hex = true,
+		enable_short_hex = true,
+		enable_rgb = true,
+		enable_hsl = true,
+		enable_var_usage = true,
 		enable_named_colors = false,
-
-		---Highlight tailwind colors, e.g. 'bg-blue-500'
-		enable_tailwind = true,
+		enable_tailwind = false,
+		-- Exclude filetypes or buftypes from highlighting
+		exclude_filetypes = {
+			"alpha",
+			"bigfile",
+			"dap-repl",
+			"fugitive",
+			"git",
+			"notify",
+			"NvimTree",
+			"Outline",
+			"TelescopePrompt",
+			"toggleterm",
+			"undotree",
+		},
+		exclude_buftypes = {
+			"nofile",
+			"prompt",
+			"terminal",
+		},
 	})
 end
