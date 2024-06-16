2fedc0861fb0bf4fc2e43886d51cb6bec32ed8dc
jint_lzxy_ <50296129+Jint-lzxy@users.noreply.github.com>
2024-06-16 16:28:18 +0800

    feat(luasnip): support ECMAScript regexes (#1291)
    As per `:h luasnip-lsp-snippets-transformations`
    
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/plugins/completion.lua
+++ lua/modules/plugins/completion.lua
@@ -51,8 +51,9 @@ completion["hrsh7th/nvim-cmp"] = {
 	dependencies = {
 		{
 			"L3MON4D3/LuaSnip",
-			dependencies = { "rafamadriz/friendly-snippets" },
+			build = "make install_jsregexp",
 			config = require("completion.luasnip"),
+			dependencies = { "rafamadriz/friendly-snippets" },
 		},
 		{ "lukas-reineke/cmp-under-comparator" },
 		{ "saadparwaiz1/cmp_luasnip" },
