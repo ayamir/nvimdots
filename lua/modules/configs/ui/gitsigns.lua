3783ba3f6d85b915ff300c97b9891a82cca0e4ce
channinghsu <xbc1241888704@gmail.com>
2024-06-28 17:03:13 +0800

    update gitsigns.lua to get rid of warnings

--- lua/modules/configs/ui/gitsigns.lua
+++ lua/modules/configs/ui/gitsigns.lua
@@ -3,34 +3,19 @@ return function()
 	require("modules.utils").load_plugin("gitsigns", {
 		signs = {
 			add = {
-				hl = "GitSignsAdd",
 				text = "│",
-				numhl = "GitSignsAddNr",
-				linehl = "GitSignsAddLn",
 			},
 			change = {
-				hl = "GitSignsChange",
 				text = "│",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
 			},
 			delete = {
-				hl = "GitSignsDelete",
 				text = "_",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
 			},
 			topdelete = {
-				hl = "GitSignsDelete",
 				text = "‾",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
 			},
 			changedelete = {
-				hl = "GitSignsChange",
 				text = "~",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
 			},
 		},
 		on_attach = mapping.gitsigns,
