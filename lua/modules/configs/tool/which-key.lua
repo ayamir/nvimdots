aa67bf1599cf11ae871bd19d051003e689f879d6
Channing Hsu <xbc1241888704@gmail.com>
2024-07-14 01:35:02 +0800

    feat(which-key):update which-key keymaps

--- lua/modules/configs/tool/which-key.lua
+++ lua/modules/configs/tool/which-key.lua
@@ -2,43 +2,19 @@ return function()
 	local icons = {
 		ui = require("modules.utils.icons").get("ui"),
 		misc = require("modules.utils.icons").get("misc"),
-		git = require("modules.utils.icons").get("git", true),
-		cmp = require("modules.utils.icons").get("cmp", true),
 	}
 
-	require("which-key").register({
-		["<leader>"] = {
-			b = {
-				name = icons.ui.Buffer .. " Buffer",
-			},
-			d = {
-				name = icons.ui.Bug .. " Debug",
-			},
-			f = {
-				name = icons.ui.Telescope .. " Fuzzy Find",
-			},
-			g = {
-				name = icons.git.Git .. "Git",
-			},
-			l = {
-				name = icons.misc.LspAvailable .. " Lsp",
-			},
-			n = {
-				name = icons.ui.FolderOpen .. " Nvim Tree",
-			},
-			p = {
-				name = icons.ui.Package .. " Package",
-			},
-			s = {
-				name = icons.cmp.tmux .. "Session",
-			},
-			S = {
-				name = icons.ui.Search .. " Search",
-			},
-			W = {
-				name = icons.ui.Window .. " Window",
-			},
-		},
+	require("which-key").add({
+		{ "<leader>S", group = "Search" },
+		{ "<leader>W", group = "Window" },
+		{ "<leader>b", group = "Buffer" },
+		{ "<leader>d", group = "Debug" },
+		{ "<leader>f", group = "Fuzzy Find", icon = icons.ui.Telescope },
+		{ "<leader>g", group = "Git" },
+		{ "<leader>l", group = "Lsp", icon = icons.misc.LspAvailable },
+		{ "<leader>n", group = "Nvim Tree", icon = icons.ui.FolderOpen },
+		{ "<leader>p", group = "Package", icon = icons.ui.Package },
+		{ "<leader>s", group = "Session" },
 	})
 
 	require("modules.utils").load_plugin("which-key", {
