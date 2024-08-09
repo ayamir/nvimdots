c700c8aceeef2357e8eef07b9d4378c14350a9ff
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-08-09 12:32:29 +0800

    feat: `<F1>` toggles the plugin
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/keymap/lang.lua
+++ lua/keymap/lang.lua
@@ -5,6 +5,11 @@ local map_cr = bind.map_cr
 -- local map_callback = bind.map_callback
 
 local plug_map = {
+	-- Plugin render-markdown.nvim
+	["n|<F1>"] = map_cr("RenderMarkdown toggle")
+		:with_noremap()
+		:with_silent()
+		:with_desc("tool: toggle markdown preview within nvim"),
 	-- Plugin MarkdownPreview
 	["n|<F12>"] = map_cr("MarkdownPreviewToggle"):with_noremap():with_silent():with_desc("tool: Preview markdown"),
 }
