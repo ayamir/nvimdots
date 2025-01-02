0a9356734e8015208fdc99ff23841fa2c75f3097
ayamir <lgt986452565@gmail.com>
2025-01-02 16:33:51 +0800

    feat: support user customized snippets. (#1386)

--- lua/modules/configs/completion/luasnip.lua
+++ lua/modules/configs/completion/luasnip.lua
@@ -1,14 +1,20 @@
 return function()
-	local snippet_path = vim.fn.stdpath("config") .. "/snips/"
-	if not vim.tbl_contains(vim.opt.rtp:get(), snippet_path) then
-		vim.opt.rtp:append(snippet_path)
-	end
+	local vim_path = require("core.global").vim_path
+	local snippet_path = vim_path .. "/snips/"
+	local user_snippet_path = vim_path .. "/lua/user/snips/"
 
 	require("modules.utils").load_plugin("luasnip", {
 		history = true,
 		update_events = "TextChanged,TextChangedI",
 		delete_check_events = "TextChanged,InsertLeave",
 	}, false, require("luasnip").config.set_config)
+
+	require("luasnip.loaders.from_vscode").lazy_load({
+		paths = {
+			snippet_path,
+			user_snippet_path,
+		},
+	})
 	require("luasnip.loaders.from_lua").lazy_load()
 	require("luasnip.loaders.from_vscode").lazy_load()
 	require("luasnip.loaders.from_snipmate").lazy_load()
