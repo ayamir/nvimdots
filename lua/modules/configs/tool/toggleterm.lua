1a1156e5c403d24291f016407386216427960cd6
ayamir <lgt986452565@gmail.com>
2025-01-09 20:56:49 +0800

    perf: remove toggleterm workaround.

--- lua/modules/configs/tool/toggleterm.lua
+++ lua/modules/configs/tool/toggleterm.lua
@@ -8,21 +8,11 @@ return function()
 				return vim.o.columns * 0.40
 			end
 		end,
-		on_open = function(term)
+		on_open = function()
 			-- Prevent infinite calls from freezing neovim.
 			-- Only set these options specific to this terminal buffer.
 			vim.api.nvim_set_option_value("foldmethod", "manual", { scope = "local" })
 			vim.api.nvim_set_option_value("foldexpr", "0", { scope = "local" })
-
-			-- Prevent horizontal terminal from obscuring `nvim-tree`.
-			local api = require("nvim-tree.api")
-			local tree = require("nvim-tree.view")
-			if tree.is_visible() and term.direction == "horizontal" then
-				local width = vim.fn.winwidth(tree.get_winnr())
-				api.tree.toggle()
-				tree.View.width = width
-				api.tree.toggle(false, true)
-			end
 		end,
 		highlights = {
 			Normal = {
