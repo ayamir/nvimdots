3e7dfb2edb06026e3b29bdd973448a66dd7cec70
ayamir <lgt986452565@gmail.com>
2025-01-13 10:59:52 +0800

    fix: toggle edgy left.

--- lua/keymap/tool.lua
+++ lua/keymap/tool.lua
@@ -7,13 +7,20 @@ local vim_path = require("core.global").vim_path
 require("keymap.helpers")
 
 local plug_map = {
+	-- Plugin: edgy
+	["n|<C-n>"] = map_callback(function()
+			require("edgy").toggle("left")
+		end)
+		:with_noremap()
+		:with_silent()
+		:with_desc("filetree: Toggle"),
+
 	-- Plugin: vim-fugitive
 	["n|gps"] = map_cr("G push"):with_noremap():with_silent():with_desc("git: Push"),
 	["n|gpl"] = map_cr("G pull"):with_noremap():with_silent():with_desc("git: Pull"),
 	["n|<leader>gG"] = map_cu("Git"):with_noremap():with_silent():with_desc("git: Open git-fugitive"),
 
 	-- Plugin: nvim-tree
-	["n|<C-n>"] = map_cr("NvimTreeToggle"):with_noremap():with_silent():with_desc("filetree: Toggle"),
 	["n|<leader>nf"] = map_cr("NvimTreeFindFile"):with_noremap():with_silent():with_desc("filetree: Find file"),
 	["n|<leader>nr"] = map_cr("NvimTreeRefresh"):with_noremap():with_silent():with_desc("filetree: Refresh"),
 
