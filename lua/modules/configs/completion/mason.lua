00b79f340a6f00617710f3afcd07bdd296ea7356
jint_lzxy_ <50296129+Jint-lzxy@users.noreply.github.com>
2025-03-03 13:50:58 +0800

    fix(mason): add nil check to on_stderr callback in install-pylsp-plugins (#1431)
    The luv doc says the on_stderr callback is also called whenever the program
    exits, not just when smth is written to stderr. So we should only notify if
    there's a real error in msg_stream.
    
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/completion/mason.lua
+++ lua/modules/configs/completion/mason.lua
@@ -83,7 +83,9 @@ M.setup = function()
 						)
 					end,
 					on_stderr = function(_, msg_stream)
-						vim.notify(msg_stream, vim.log.levels.ERROR, { title = "[lsp] Install Failure" })
+						if msg_stream then
+							vim.notify(msg_stream, vim.log.levels.ERROR, { title = "[lsp] Install Failure" })
+						end
 					end,
 				})
 				:start()
