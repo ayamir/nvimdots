7d3a4e35da040ca02ef941019e313979bbacabe9
jint_lzxy_ <50296129+Jint-lzxy@users.noreply.github.com>
2025-04-05 06:17:05 -0700

    chore(configs): remove useless legacy configs (#1428)
    * chore(configs): remove useless legacy configs
    
    This commit got rid of some legacy configs that literally did nothing (fr this
    time lol).
    
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
    
    * feat: define custom keymaps
    
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/editor/grug-far.lua
+++ lua/modules/configs/editor/grug-far.lua
@@ -1,6 +1,8 @@
 return function()
-	vim.g.maplocalleader = ","
 	require("modules.utils").load_plugin("grug-far", {
+		windowCreationCommand = "bot split",
+		disableBufferLineNumbers = false,
+		icons = { enabled = true },
 		engine = "ripgrep",
 		engines = {
 			ripgrep = {
@@ -11,10 +13,30 @@ return function()
 				},
 			},
 		},
-		windowCreationCommand = "bot split",
-		disableBufferLineNumbers = false,
-		icons = {
-			enabled = true,
+		keymaps = {
+			replace = { n = "R" },
+			qflist = { n = "Q" },
+			syncLocations = { n = "S" },
+			syncLine = { n = "L" },
+			close = { n = "C" },
+			historyOpen = { n = "T" },
+			historyAdd = { n = "A" },
+			refresh = { n = "F" },
+			openLocation = { n = "O" },
+			openNextLocation = { n = "<Down>" },
+			openPrevLocation = { n = "<Up>" },
+			gotoLocation = { n = "<Enter>" },
+			pickHistoryEntry = { n = "<Enter>" },
+			abort = { n = "B" },
+			help = { n = "g?" },
+			toggleShowCommand = { n = "P" },
+			swapEngine = { n = "E" },
+			previewLocation = { n = "I" },
+			swapReplacementInterpreter = { n = "X" },
+			applyNext = { n = "J" },
+			applyPrev = { n = "K" },
+			nextInput = { n = "<Tab>" },
+			prevInput = { n = "<S-Tab>" },
 		},
 	})
 end
