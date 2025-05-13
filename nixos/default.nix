2a914ecda4d3d4883123dff1ce9e3b6c9f50298c
misumisumi <sumiharu@misumi-sumi.com>
2025-05-13 20:54:24 +0900

    style(nix): formatting by nixfmt

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,4 +1,3 @@
-# NOTE: to add more language support, make a directory under `nixos`, followed by the language name and `default.nix`. See `dotnet/default.nix` for example.
 {
   imports = [
     ./neovim
--- nixos/testEnv.nix
+++ nixos/testEnv.nix
@@ -1,29 +1,31 @@
 { inputs, pkgs, ... }:
 let
-  testSettings = { config, ... }: {
-    warnings = [
-      "home-manager version: ${config.home.version.release}"
-    ];
-    home = {
-      username = "hm-user";
-      homeDirectory = "/home/hm-user";
-      stateVersion = config.home.version.release;
-    };
-    xdg.enable = true;
-    programs = {
-      home-manager.enable = true;
-      git.enable = true;
-      neovim = {
-        enable = true;
-        nvimdots = {
+  testSettings =
+    { config, ... }:
+    {
+      warnings = [
+        "home-manager version: ${config.home.version.release}"
+      ];
+      home = {
+        username = "hm-user";
+        homeDirectory = "/home/hm-user";
+        stateVersion = config.home.version.release;
+      };
+      xdg.enable = true;
+      programs = {
+        home-manager.enable = true;
+        git.enable = true;
+        neovim = {
           enable = true;
-          setBuildEnv = true;
-          withBuildTools = true;
-          withHaskell = true;
+          nvimdots = {
+            enable = true;
+            setBuildEnv = true;
+            withBuildTools = true;
+            withHaskell = true;
+          };
         };
       };
     };
-  };
 in
 inputs.home-manager.lib.homeManagerConfiguration {
   inherit pkgs;
