ba552b7b8621f8ed5812d0eeb651020b7ce48c2f
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2025-07-22 17:20:52 +0800

    refactor(plugins): simplify single dependency declarations and comment wording
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/plugins/lang.lua
+++ lua/modules/plugins/lang.lua
@@ -13,20 +13,20 @@ lang["ray-x/go.nvim"] = {
 	ft = { "go", "gomod", "gosum" },
 	build = ":GoInstallBinaries",
 	config = require("lang.go"),
-	dependencies = { "ray-x/guihua.lua" },
+	dependencies = "ray-x/guihua.lua",
 }
 lang["mrcjkb/rustaceanvim"] = {
 	lazy = true,
 	ft = "rust",
 	version = "*",
 	init = require("lang.rust"),
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = "nvim-lua/plenary.nvim",
 }
 lang["Saecki/crates.nvim"] = {
 	lazy = true,
 	event = "BufReadPost Cargo.toml",
 	config = require("lang.crates"),
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = "nvim-lua/plenary.nvim",
 }
 lang["MeanderingProgrammer/render-markdown.nvim"] = {
 	lazy = true,
--- lua/modules/plugins/tool.lua
+++ lua/modules/plugins/tool.lua
@@ -38,8 +38,8 @@ tool["ibhagwan/smartyank.nvim"] = {
 }
 tool["michaelb/sniprun"] = {
 	lazy = true,
-	-- You need to cd to `~/.local/share/nvim/site/lazy/sniprun/` and execute `bash ./install.sh`
-	-- if you encounter an error about no executable sniprun found.
+	-- If you see an error about a missing SnipRun executable,
+	-- run `bash ./install.sh` inside `~/.local/share/nvim/site/lazy/sniprun/`.
 	build = "bash ./install.sh",
 	cmd = { "SnipRun", "SnipReset", "SnipInfo" },
 	config = require("tool.sniprun"),
@@ -70,7 +70,7 @@ tool["gelguy/wilder.nvim"] = {
 	lazy = true,
 	event = "CmdlineEnter",
 	config = require("tool.wilder"),
-	dependencies = { "romgrk/fzy-lua-native" },
+	dependencies = "romgrk/fzy-lua-native",
 }
 if settings.search_backend == "fzf" then
 	-- requires the fzf binary to be installed
@@ -135,14 +135,12 @@ tool["mfussenegger/nvim-dap"] = {
 	},
 	config = require("tool.dap"),
 	dependencies = {
+		{ "jay-babu/mason-nvim-dap.nvim" },
 		{
 			"rcarriga/nvim-dap-ui",
+			dependencies = "nvim-neotest/nvim-nio",
 			config = require("tool.dap.dapui"),
-			dependencies = {
-				"nvim-neotest/nvim-nio",
-			},
 		},
-		{ "jay-babu/mason-nvim-dap.nvim" },
 	},
 }
 
