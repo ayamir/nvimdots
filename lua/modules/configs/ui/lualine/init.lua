40d7cc0abbb8645efb511b10c5ff45c5203da386
ayamir <lgt986452565@gmail.com>
2025-07-27 20:49:13 +0800

    fix: remove unused function.

--- lua/modules/configs/ui/lualine/init.lua
+++ lua/modules/configs/ui/lualine/init.lua
@@ -26,10 +26,6 @@ vim.api.nvim_create_autocmd("LspProgress", {
 	end,
 })
 
-local lsp_msg = function()
-	return vim.o.columns < 120 and "" or state.lsp_msg
-end
-
 return function()
 	local has_catppuccin = vim.g.colors_name:find("catppuccin") ~= nil
 	local colors = require("modules.utils").get_palette()
