aa2c4409cd51a0ca152733128efd3c152a93d6f4
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2025-08-04 00:39:46 +0800

    feat(mason)!: migrate to v2.x
    Just wrapped up going thru all the changes in mason 2.x (and nvim 0.11 too)!
    This should be a follow up to #1466 and (hopefully) fix all the issues related
    to the updated LSP API (like servers not spawning and other weirdness) and works
    together w #1496.
    
    Acc the main issue was that some mason package names don't match their names in
    lspconfig, so just using `lsp_deps` directly works for most, but breaks a bunch
    of others (tldr: that mismatch is what was causing all the edge cases and broken
    behavior).
    
    I also went ahead and enabled `automatic_installation` because why not lol means
    we don't have to manually call `vim.lsp.enable()` for everything, which might
    save a tiny bit of perf too.
    
    Didn't test this super thoroughly yet so pls lmk if anything's still broken!
    
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/completion/lsp.lua
+++ lua/modules/configs/completion/lsp.lua
@@ -7,14 +7,16 @@ return function()
 	local opts = {
 		capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 	}
-	-- Setup lsps that are not supported by `mason.nvim` but supported by `nvim-lspconfig` here.
+	-- Configure LSPs that are not supported by `mason.nvim` but are available in `nvim-lspconfig` here.
+	-- Call |vim.lsp.config()| FOLLOWED BY |vim.lsp.enable()| to ensure the server starts automatically.
 	if vim.fn.executable("dart") == 1 then
 		local ok, _opts = pcall(require, "user.configs.lsp-servers.dartls")
 		if not ok then
 			_opts = require("completion.servers.dartls")
 		end
 		local final_opts = vim.tbl_deep_extend("keep", _opts, opts)
-		nvim_lsp.dartls.setup(final_opts)
+		vim.lsp.config("dartls", final_opts)
+		vim.lsp.enable("dartls")
 	end
 
 	pcall(require, "user.configs.lsp")
