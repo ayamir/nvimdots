e82cc2271aa46b8f09e37c649c0e89121f4eb316
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2025-10-09 17:34:34 +0800

    added(python): align with previous ruff config.
    As mentioned in #1528, keep the ruff config align with previous config.
    
    This includes:
    1. set default line length to 88, as `black`'s default behavior
    2. align with previous linter config
    
    As for the pyrefly lsp config, I guess the default is good to go.

---
+++ lua/modules/configs/completion/servers/ruff.lua
@@ -0,0 +1,56 @@
+---@brief
+---
+--- https://github.com/astral-sh/ruff
+---
+--- A Language Server Protocol implementation for Ruff, an extremely fast Python linter and code formatter, written in Rust. It can be installed via `pip`.
+---
+--- ```sh
+--- pip install ruff
+--- ```
+---
+--- **Available in Ruff `v0.4.5` in beta and stabilized in Ruff `v0.5.3`.**
+---
+--- This is the new built-in language server written in Rust. It supports the same feature set as `ruff-lsp`, but with superior performance and no installation required. Note that the `ruff-lsp` server will continue to be maintained until further notice.
+---
+--- Server settings can be provided via:
+---
+--- ```lua
+--- vim.lsp.config('ruff', {
+---   init_options = {
+---     settings = {
+---       -- Server settings should go here
+---     }
+---   }
+--- })
+--- ```
+---
+--- Refer to the [documentation](https://docs.astral.sh/ruff/editors/) for more details.
+
+---@type vim.lsp.Config
+return {
+	cmd = { "ruff", "server" },
+	filetypes = { "python" },
+	root_markers = { "pyproject.toml", "ruff.toml", ".ruff.toml", ".git" },
+
+	-- the following are added by nvimdots
+	settings = {
+		init_options = {
+			settings = {
+				lint = {
+					select = {
+						-- enable: pycodestyle
+						"E",
+						-- enable: pyflakes
+						"F",
+					},
+					extendSelect = {
+						-- enable: isort
+						"I",
+					},
+					-- the same line length as black
+					lineLength = 88,
+				},
+			},
+		},
+	},
+}
