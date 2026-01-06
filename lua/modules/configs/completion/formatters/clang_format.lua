f720913b62537fa2076b85bbb041b7ad66bc499e
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2026-01-06 18:28:16 +0800

    fix: style lua ci/cd

--- lua/modules/configs/completion/formatters/clang_format.lua
+++ lua/modules/configs/completion/formatters/clang_format.lua
@@ -2,6 +2,6 @@ local project_root = vim.fs.root(vim.fn.getcwd(), { ".git" }) or vim.fn.getcwd()
 
 -- check if ".clang-format" config exists, if it does, use the config instead of default
 if not vim.uv.fs_stat(vim.fs.joinpath(project_root, ".clang-format")) then
-    return { "-style={ BasedOnStyle: LLVM, IndentWidth: 4 }" }
+	return { "-style={ BasedOnStyle: LLVM, IndentWidth: 4 }" }
 end
 return {}
